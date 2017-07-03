ddfaacda004cf446d188b167f113e8475b893df0
Karthikeyan Ravikumar <kartefact@gmail.com>
2017-07-03 05:42:30 +0530

    Added 2 tweets

--- data/js/tweets/2017_07.js
+++ data/js/tweets/2017_07.js
@@ -1,6 +1,551 @@
 Grailbird.data.tweets_2017_07 = 
 [
 	{
+		"created_at": "Sun Jul 02 13:17:59 +0000 2017",
+		"id": 881502212924391400,
+		"id_str": "881502212924391429",
+		"text": "RT @arunk_: What is the procedure to meet Norbert? https://t.co/0CZMVGScOK",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "arunk_",
+					"name": "Punjai Kumaarasaamy",
+					"id": 20760288,
+					"id_str": "20760288",
+					"indices": [
+						3,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 881140422948540400,
+					"id_str": "881140422948540416",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/881140422948540416/pu/img/hfkjKqaAYXKf8u8V.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/881140422948540416/pu/img/hfkjKqaAYXKf8u8V.jpg",
+					"url": "https://t.co/0CZMVGScOK",
+					"display_url": "pic.twitter.com/0CZMVGScOK",
+					"expanded_url": "https://twitter.com/arunk_/status/881142022064689152/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 340,
+							"h": 340,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 881142022064689200,
+					"source_status_id_str": "881142022064689152",
+					"source_user_id": 20760288,
+					"source_user_id_str": "20760288"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 881140422948540400,
+					"id_str": "881140422948540416",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/881140422948540416/pu/img/hfkjKqaAYXKf8u8V.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/881140422948540416/pu/img/hfkjKqaAYXKf8u8V.jpg",
+					"url": "https://t.co/0CZMVGScOK",
+					"display_url": "pic.twitter.com/0CZMVGScOK",
+					"expanded_url": "https://twitter.com/arunk_/status/881142022064689152/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 340,
+							"h": 340,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 881142022064689200,
+					"source_status_id_str": "881142022064689152",
+					"source_user_id": 20760288,
+					"source_user_id_str": "20760288",
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 43777,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/881140422948540416/pu/pl/Yr6wkBByZfaPtR8b.m3u8"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/881140422948540416/pu/vid/480x480/lXpP2qid92xbUXbN.mp4"
+							},
+							{
+								"bitrate": 320000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/881140422948540416/pu/vid/240x240/LowSJ0JGyqXGROMF.mp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 20760288,
+							"id_str": "20760288",
+							"name": "Punjai Kumaarasaamy",
+							"screen_name": "arunk_",
+							"location": "Bangalore, India",
+							"description": "Thennagaththu Jon Snow cos I know nothing. Naattaamai in most WhatsApp groups. RT spam of funny and crazy stuff.",
+							"url": "https://t.co/nUH8n9cwHp",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "https://t.co/nUH8n9cwHp",
+											"expanded_url": "http://www.trakt.tv/users/arunjeevi",
+											"display_url": "trakt.tv/users/arunjeevi",
+											"indices": [
+												0,
+												23
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 629,
+							"friends_count": 1179,
+							"listed_count": 39,
+							"created_at": "Fri Feb 13 10:03:20 +0000 2009",
+							"favourites_count": 734,
+							"utc_offset": 19800,
+							"time_zone": "New Delhi",
+							"geo_enabled": false,
+							"verified": false,
+							"statuses_count": 96301,
+							"lang": "en",
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "709397",
+							"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/451634767557963776/M8OrJdbf.jpeg",
+							"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/451634767557963776/M8OrJdbf.jpeg",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/847175944737636352/-d_4Fz26_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/847175944737636352/-d_4Fz26_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/20760288/1454345749",
+							"profile_link_color": "1B95E0",
+							"profile_sidebar_border_color": "000000",
+							"profile_sidebar_fill_color": "A0C5C7",
+							"profile_text_color": "333333",
+							"profile_use_background_image": true,
+							"has_extended_profile": true,
+							"default_profile": false,
+							"default_profile_image": false,
+							"following": true,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "The only superpower that humans need is empathy.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1590,
+			"friends_count": 504,
+			"listed_count": 54,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 9484,
+			"utc_offset": 19800,
+			"time_zone": "Asia/Calcutta",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 97997,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/871447740357672961/fvo44w3e_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/871447740357672961/fvo44w3e_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1497288569",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Jul 01 13:26:43 +0000 2017",
+			"id": 881142022064689200,
+			"id_str": "881142022064689152",
+			"text": "What is the procedure to meet Norbert? https://t.co/0CZMVGScOK",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 881140422948540400,
+						"id_str": "881140422948540416",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/881140422948540416/pu/img/hfkjKqaAYXKf8u8V.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/881140422948540416/pu/img/hfkjKqaAYXKf8u8V.jpg",
+						"url": "https://t.co/0CZMVGScOK",
+						"display_url": "pic.twitter.com/0CZMVGScOK",
+						"expanded_url": "https://twitter.com/arunk_/status/881142022064689152/video/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 480,
+								"h": 480,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 340,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 480,
+								"h": 480,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 881140422948540400,
+						"id_str": "881140422948540416",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/881140422948540416/pu/img/hfkjKqaAYXKf8u8V.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/881140422948540416/pu/img/hfkjKqaAYXKf8u8V.jpg",
+						"url": "https://t.co/0CZMVGScOK",
+						"display_url": "pic.twitter.com/0CZMVGScOK",
+						"expanded_url": "https://twitter.com/arunk_/status/881142022064689152/video/1",
+						"type": "video",
+						"sizes": {
+							"large": {
+								"w": 480,
+								"h": 480,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 340,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 480,
+								"h": 480,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 43777,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/881140422948540416/pu/pl/Yr6wkBByZfaPtR8b.m3u8"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/881140422948540416/pu/vid/480x480/lXpP2qid92xbUXbN.mp4"
+								},
+								{
+									"bitrate": 320000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/881140422948540416/pu/vid/240x240/LowSJ0JGyqXGROMF.mp4"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 20760288,
+				"id_str": "20760288",
+				"name": "Punjai Kumaarasaamy",
+				"screen_name": "arunk_",
+				"location": "Bangalore, India",
+				"description": "Thennagaththu Jon Snow cos I know nothing. Naattaamai in most WhatsApp groups. RT spam of funny and crazy stuff.",
+				"url": "https://t.co/nUH8n9cwHp",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/nUH8n9cwHp",
+								"expanded_url": "http://www.trakt.tv/users/arunjeevi",
+								"display_url": "trakt.tv/users/arunjeevi",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 629,
+				"friends_count": 1179,
+				"listed_count": 39,
+				"created_at": "Fri Feb 13 10:03:20 +0000 2009",
+				"favourites_count": 734,
+				"utc_offset": 19800,
+				"time_zone": "New Delhi",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 96301,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "709397",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/451634767557963776/M8OrJdbf.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/451634767557963776/M8OrJdbf.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/847175944737636352/-d_4Fz26_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/847175944737636352/-d_4Fz26_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/20760288/1454345749",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "A0C5C7",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Sun Jul 02 05:28:21 +0000 2017",
+		"id": 881384024425484300,
+		"id_str": "881384024425484289",
+		"text": "@mudnsky Oho... So that's what you've been up to. 🆒",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mudnsky",
+					"name": "Mud n Sky",
+					"id": 1685849366,
+					"id_str": "1685849366",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 881131268938670100,
+		"in_reply_to_status_id_str": "881131268938670080",
+		"in_reply_to_user_id": 1685849366,
+		"in_reply_to_user_id_str": "1685849366",
+		"in_reply_to_screen_name": "mudnsky",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "The only superpower that humans need is empathy.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1590,
+			"friends_count": 504,
+			"listed_count": 54,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 9484,
+			"utc_offset": 19800,
+			"time_zone": "Asia/Calcutta",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 97997,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/871447740357672961/fvo44w3e_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/871447740357672961/fvo44w3e_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1497288569",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Jul 01 13:41:20 +0000 2017",
 		"id": 881145700821512200,
 		"id_str": "881145700821512192",
