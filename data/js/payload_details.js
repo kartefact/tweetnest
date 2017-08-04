5f1dd024ecc7781d1d6318226173e9941f539cf1
Karthikeyan Ravikumar <kartefact@gmail.com>
2017-08-04 05:42:29 +0530

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 98408,
-	"created_at": "Thu Aug 03 00:12:29 +0000 2017",
+	"tweets": 98410,
+	"created_at": "Fri Aug 04 00:12:28 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,477 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Thu Aug 03 09:03:12 +0000 2017",
+		"id": 893034505681813500,
+		"id_str": "893034505681813506",
+		"text": "RT @ruchi18: EPIC! https://t.co/gtDUzERtEt",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ruchi18",
+					"name": "Mrs Khandhar",
+					"id": 15350382,
+					"id_str": "15350382",
+					"indices": [
+						3,
+						11
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/gtDUzERtEt",
+					"expanded_url": "https://twitter.com/_youhadonejob1/status/893033156915417088",
+					"display_url": "twitter.com/_youhadonejob1…",
+					"indices": [
+						19,
+						42
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "The only superpower that humans need is empathy.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1593,
+			"friends_count": 510,
+			"listed_count": 54,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 9567,
+			"utc_offset": 19800,
+			"time_zone": "Asia/Calcutta",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 98123,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/886655834943442944/_96VrJPa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/886655834943442944/_96VrJPa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1497288569",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Aug 03 09:02:50 +0000 2017",
+			"id": 893034415068241900,
+			"id_str": "893034415068241920",
+			"text": "EPIC! https://t.co/gtDUzERtEt",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/gtDUzERtEt",
+						"expanded_url": "https://twitter.com/_youhadonejob1/status/893033156915417088",
+						"display_url": "twitter.com/_youhadonejob1…",
+						"indices": [
+							6,
+							29
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15350382,
+				"id_str": "15350382",
+				"name": "Mrs Khandhar",
+				"screen_name": "ruchi18",
+				"location": "London-Mumbai-Melbourne",
+				"description": "Hi Priya, haan main, magar woh, suno toh, tumne toh, lekin main, kabse kya, main, Aakash ke yahan. Hello?",
+				"url": "https://t.co/WccHVmH1Rj",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WccHVmH1Rj",
+								"expanded_url": "http://about.me/ruchi18",
+								"display_url": "about.me/ruchi18",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2052,
+				"friends_count": 500,
+				"listed_count": 72,
+				"created_at": "Tue Jul 08 05:46:13 +0000 2008",
+				"favourites_count": 1390,
+				"utc_offset": 3600,
+				"time_zone": "London",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 37948,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "352726",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/798971241310945280/oKPsSq1Z_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/798971241310945280/oKPsSq1Z_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15350382/1457455498",
+				"profile_link_color": "08BDFF",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "99CC33",
+				"profile_text_color": "3E4415",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 893033156915417100,
+			"quoted_status_id_str": "893033156915417088",
+			"quoted_status": {
+				"created_at": "Thu Aug 03 08:57:51 +0000 2017",
+				"id": 893033156915417100,
+				"id_str": "893033156915417088",
+				"text": "Never play with superglue. https://t.co/mydtVaLMVA",
+				"truncated": false,
+				"entities": {
+					"hashtags": [],
+					"symbols": [],
+					"user_mentions": [],
+					"urls": [],
+					"media": [
+						{
+							"id": 893033155334197200,
+							"id_str": "893033155334197249",
+							"indices": [
+								27,
+								50
+							],
+							"media_url": "http://pbs.twimg.com/media/DGSw7FxXUAE5kDZ.jpg",
+							"media_url_https": "https://pbs.twimg.com/media/DGSw7FxXUAE5kDZ.jpg",
+							"url": "https://t.co/mydtVaLMVA",
+							"display_url": "pic.twitter.com/mydtVaLMVA",
+							"expanded_url": "https://twitter.com/_youhadonejob1/status/893033156915417088/photo/1",
+							"type": "photo",
+							"sizes": {
+								"large": {
+									"w": 490,
+									"h": 487,
+									"resize": "fit"
+								},
+								"thumb": {
+									"w": 150,
+									"h": 150,
+									"resize": "crop"
+								},
+								"medium": {
+									"w": 490,
+									"h": 487,
+									"resize": "fit"
+								},
+								"small": {
+									"w": 490,
+									"h": 487,
+									"resize": "fit"
+								}
+							}
+						}
+					]
+				},
+				"extended_entities": {
+					"media": [
+						{
+							"id": 893033155334197200,
+							"id_str": "893033155334197249",
+							"indices": [
+								27,
+								50
+							],
+							"media_url": "http://pbs.twimg.com/media/DGSw7FxXUAE5kDZ.jpg",
+							"media_url_https": "https://pbs.twimg.com/media/DGSw7FxXUAE5kDZ.jpg",
+							"url": "https://t.co/mydtVaLMVA",
+							"display_url": "pic.twitter.com/mydtVaLMVA",
+							"expanded_url": "https://twitter.com/_youhadonejob1/status/893033156915417088/photo/1",
+							"type": "photo",
+							"sizes": {
+								"large": {
+									"w": 490,
+									"h": 487,
+									"resize": "fit"
+								},
+								"thumb": {
+									"w": 150,
+									"h": 150,
+									"resize": "crop"
+								},
+								"medium": {
+									"w": 490,
+									"h": 487,
+									"resize": "fit"
+								},
+								"small": {
+									"w": 490,
+									"h": 487,
+									"resize": "fit"
+								}
+							}
+						}
+					]
+				},
+				"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+				"in_reply_to_status_id": null,
+				"in_reply_to_status_id_str": null,
+				"in_reply_to_user_id": null,
+				"in_reply_to_user_id_str": null,
+				"in_reply_to_screen_name": null,
+				"user": {
+					"id": 3898598357,
+					"id_str": "3898598357",
+					"name": "You Had One Job",
+					"screen_name": "_youhadonejob1",
+					"location": "yhojob1gmail.com",
+					"description": "You had one job and messed up plus more fun pics and vids.I do not own any content just a fan/parody*and will remove content @ copyright holders request,email",
+					"url": "https://t.co/jRKY0FMPFF",
+					"entities": {
+						"url": {
+							"urls": [
+								{
+									"url": "https://t.co/jRKY0FMPFF",
+									"expanded_url": "https://m.facebook.com/Funny-pics-377802085710502/",
+									"display_url": "m.facebook.com/Funny-pics-377…",
+									"indices": [
+										0,
+										23
+									]
+								}
+							]
+						},
+						"description": {
+							"urls": []
+						}
+					},
+					"protected": false,
+					"followers_count": 236400,
+					"friends_count": 239,
+					"listed_count": 1609,
+					"created_at": "Thu Oct 08 10:23:15 +0000 2015",
+					"favourites_count": 194,
+					"utc_offset": null,
+					"time_zone": null,
+					"geo_enabled": false,
+					"verified": false,
+					"statuses_count": 6382,
+					"lang": "en-GB",
+					"contributors_enabled": false,
+					"is_translator": false,
+					"is_translation_enabled": false,
+					"profile_background_color": "C0DEED",
+					"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+					"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+					"profile_background_tile": false,
+					"profile_image_url": "http://pbs.twimg.com/profile_images/703223013551022080/u6mVd-Tk_normal.jpg",
+					"profile_image_url_https": "https://pbs.twimg.com/profile_images/703223013551022080/u6mVd-Tk_normal.jpg",
+					"profile_banner_url": "https://pbs.twimg.com/profile_banners/3898598357/1444300597",
+					"profile_link_color": "1DA1F2",
+					"profile_sidebar_border_color": "C0DEED",
+					"profile_sidebar_fill_color": "DDEEF6",
+					"profile_text_color": "333333",
+					"profile_use_background_image": true,
+					"has_extended_profile": false,
+					"default_profile": true,
+					"default_profile_image": false,
+					"following": false,
+					"follow_request_sent": false,
+					"notifications": false,
+					"translator_type": "none"
+				},
+				"geo": null,
+				"coordinates": null,
+				"place": null,
+				"contributors": null,
+				"is_quote_status": false,
+				"retweet_count": 1047,
+				"favorite_count": 2524,
+				"favorited": false,
+				"retweeted": false,
+				"possibly_sensitive": false,
+				"lang": "en"
+			},
+			"retweet_count": 2,
+			"favorite_count": 1,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": true,
+		"quoted_status_id": 893033156915417100,
+		"quoted_status_id_str": "893033156915417088",
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Aug 03 05:25:23 +0000 2017",
+		"id": 892979688993210400,
+		"id_str": "892979688993210368",
+		"text": "@krishnapermi @probably_this Retirement will be in Mysore through NICE road after working our backs off at Electronics city :P",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "krishnapermi",
+					"name": "Krishna Pérmi",
+					"id": 487401567,
+					"id_str": "487401567",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "probably_this",
+					"name": "D",
+					"id": 705436470132260900,
+					"id_str": "705436470132260864",
+					"indices": [
+						14,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 892723000776171500,
+		"in_reply_to_status_id_str": "892723000776171521",
+		"in_reply_to_user_id": 487401567,
+		"in_reply_to_user_id_str": "487401567",
+		"in_reply_to_screen_name": "krishnapermi",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "The only superpower that humans need is empathy.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1593,
+			"friends_count": 510,
+			"listed_count": 54,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 9567,
+			"utc_offset": 19800,
+			"time_zone": "Asia/Calcutta",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 98123,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/886655834943442944/_96VrJPa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/886655834943442944/_96VrJPa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1497288569",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Aug 02 11:16:24 +0000 2017",
 		"id": 892705640224665600,
 		"id_str": "892705640224665600",
