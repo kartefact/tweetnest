68da0ad8bc56f87be1b09f1f7ab4cee4f434b83f
Karthikeyan Ravikumar <kartefact@gmail.com>
2018-04-13 05:42:30 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 99433,
-	"created_at": "Thu Apr 12 00:12:29 +0000 2018",
+	"tweets": 99434,
+	"created_at": "Fri Apr 13 00:12:29 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,350 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Thu Apr 12 18:03:02 +0000 2018",
+		"id": 984492102405980200,
+		"id_str": "984492102405980162",
+		"text": "RT @hvgoenka: They said.. we wonder what vegetarians eat! https://t.co/yKFUZtD80y",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hvgoenka",
+					"name": "Harsh Goenka",
+					"id": 39488770,
+					"id_str": "39488770",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 983761028201795600,
+					"id_str": "983761028201795586",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/DacFcWXXUAIhNhH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DacFcWXXUAIhNhH.jpg",
+					"url": "https://t.co/yKFUZtD80y",
+					"display_url": "pic.twitter.com/yKFUZtD80y",
+					"expanded_url": "https://twitter.com/hvgoenka/status/983761042248556544/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 643,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 643,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 455,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 983761042248556500,
+					"source_status_id_str": "983761042248556544",
+					"source_user_id": 39488770,
+					"source_user_id_str": "39488770"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 983761028201795600,
+					"id_str": "983761028201795586",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/DacFcWXXUAIhNhH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DacFcWXXUAIhNhH.jpg",
+					"url": "https://t.co/yKFUZtD80y",
+					"display_url": "pic.twitter.com/yKFUZtD80y",
+					"expanded_url": "https://twitter.com/hvgoenka/status/983761042248556544/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 643,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 643,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 455,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 983761042248556500,
+					"source_status_id_str": "983761042248556544",
+					"source_user_id": 39488770,
+					"source_user_id_str": "39488770"
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "Clarity is everything!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1622,
+			"friends_count": 526,
+			"listed_count": 54,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10120,
+			"utc_offset": 19800,
+			"time_zone": "Asia/Calcutta",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 98866,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/590537116137979904/ktDWab05.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/970356405625647106/_WGfMH3H_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/970356405625647106/_WGfMH3H_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1518968138",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Apr 10 17:38:04 +0000 2018",
+			"id": 983761042248556500,
+			"id_str": "983761042248556544",
+			"text": "They said.. we wonder what vegetarians eat! https://t.co/yKFUZtD80y",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 983761028201795600,
+						"id_str": "983761028201795586",
+						"indices": [
+							44,
+							67
+						],
+						"media_url": "http://pbs.twimg.com/media/DacFcWXXUAIhNhH.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DacFcWXXUAIhNhH.jpg",
+						"url": "https://t.co/yKFUZtD80y",
+						"display_url": "pic.twitter.com/yKFUZtD80y",
+						"expanded_url": "https://twitter.com/hvgoenka/status/983761042248556544/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 643,
+								"h": 960,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 643,
+								"h": 960,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 455,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 983761028201795600,
+						"id_str": "983761028201795586",
+						"indices": [
+							44,
+							67
+						],
+						"media_url": "http://pbs.twimg.com/media/DacFcWXXUAIhNhH.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DacFcWXXUAIhNhH.jpg",
+						"url": "https://t.co/yKFUZtD80y",
+						"display_url": "pic.twitter.com/yKFUZtD80y",
+						"expanded_url": "https://twitter.com/hvgoenka/status/983761042248556544/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 643,
+								"h": 960,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 643,
+								"h": 960,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 455,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 39488770,
+				"id_str": "39488770",
+				"name": "Harsh Goenka",
+				"screen_name": "hvgoenka",
+				"location": "Mumbai",
+				"description": "Chairman - RPG Enterprises. Loves people, food, sports, art.",
+				"url": "https://t.co/VKD5ha8rSh",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VKD5ha8rSh",
+								"expanded_url": "http://www.rpggroup.com",
+								"display_url": "rpggroup.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1527457,
+				"friends_count": 98,
+				"listed_count": 313,
+				"created_at": "Tue May 12 12:34:16 +0000 2009",
+				"favourites_count": 39034,
+				"utc_offset": 19800,
+				"time_zone": "New Delhi",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 3121,
+				"lang": "en-gb",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/895728174910251009/q03N4ZpE_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/895728174910251009/q03N4ZpE_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/39488770/1522216455",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 527,
+			"favorite_count": 1405,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 527,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Apr 11 07:40:47 +0000 2018",
 		"id": 983973119948984300,
 		"id_str": "983973119948984321",
