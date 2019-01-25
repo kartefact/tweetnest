78bf40e36152b2d927aa931da24604de70d4b71e
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-01-25 05:42:30 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100561,
-	"created_at": "Tue Jan 22 00:12:28 +0000 2019",
+	"tweets": 100562,
+	"created_at": "Fri Jan 25 00:12:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,320 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Thu Jan 24 21:21:34 +0000 2019",
+		"id": 1088547380159369200,
+		"id_str": "1088547380159369216",
+		"full_text": "No alternative :( All other favorites like Opera also now use the chromium engine. https://t.co/IqQOEo2KzM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IqQOEo2KzM",
+					"expanded_url": "https://twitter.com/CORSAIR/status/1088199514819768320",
+					"display_url": "twitter.com/CORSAIR/status…",
+					"indices": [
+						83,
+						106
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1555,
+			"friends_count": 530,
+			"listed_count": 55,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10655,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 99234,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1547287803",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1088199514819768300,
+		"quoted_status_id_str": "1088199514819768320",
+		"quoted_status_permalink": {
+			"url": "https://t.co/IqQOEo2KzM",
+			"expanded": "https://twitter.com/CORSAIR/status/1088199514819768320",
+			"display": "twitter.com/CORSAIR/status…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jan 23 22:19:16 +0000 2019",
+			"id": 1088199514819768300,
+			"id_str": "1088199514819768320",
+			"full_text": "This is your RAM.\n\nAnd THIS is your RAM on Chrome. https://t.co/zI9DRh53Ng",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				50
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1088199412151550000,
+						"id_str": "1088199412151549952",
+						"indices": [
+							51,
+							74
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1088199412151549952/pu/img/SESMR4G4W-Z5Q2WG.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1088199412151549952/pu/img/SESMR4G4W-Z5Q2WG.jpg",
+						"url": "https://t.co/zI9DRh53Ng",
+						"display_url": "pic.twitter.com/zI9DRh53Ng",
+						"expanded_url": "https://twitter.com/CORSAIR/status/1088199514819768320/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1088199412151550000,
+						"id_str": "1088199412151549952",
+						"indices": [
+							51,
+							74
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1088199412151549952/pu/img/SESMR4G4W-Z5Q2WG.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1088199412151549952/pu/img/SESMR4G4W-Z5Q2WG.jpg",
+						"url": "https://t.co/zI9DRh53Ng",
+						"display_url": "pic.twitter.com/zI9DRh53Ng",
+						"expanded_url": "https://twitter.com/CORSAIR/status/1088199514819768320/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 23006,
+							"variants": [
+								{
+									"bitrate": 432000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1088199412151549952/pu/vid/320x320/DHcEK-s8XLZy8L5V.mp4?tag=6"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1088199412151549952/pu/pl/NELUgpyXSWiqJC8r.m3u8?tag=6"
+								},
+								{
+									"bitrate": 1280000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1088199412151549952/pu/vid/720x720/dqDGVCU6TwcHNkFg.mp4?tag=6"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1088199412151549952/pu/vid/480x480/T4taFaTSZsQ58G7v.mp4?tag=6"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 26599573,
+				"id_str": "26599573",
+				"name": "CORSAIR",
+				"screen_name": "CORSAIR",
+				"location": "California, USA",
+				"description": "The leading source for PC components and gaming hardware since '94. Need support? Send a DM to @CorsairSupport or visit: https://t.co/S4ASPtXs6s",
+				"url": "https://t.co/KGJpZKjIz0",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/KGJpZKjIz0",
+								"expanded_url": "http://www.corsair.com",
+								"display_url": "corsair.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/S4ASPtXs6s",
+								"expanded_url": "http://corsair.com/support",
+								"display_url": "corsair.com/support",
+								"indices": [
+									121,
+									144
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1000412,
+				"friends_count": 395,
+				"listed_count": 1869,
+				"created_at": "Wed Mar 25 21:41:25 +0000 2009",
+				"favourites_count": 9359,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 12590,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1047166636027871232/nwE4ockb_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1047166636027871232/nwE4ockb_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/26599573/1538005313",
+				"profile_link_color": "FAB81E",
+				"profile_sidebar_border_color": "D4D4D4",
+				"profile_sidebar_fill_color": "D4D4D4",
+				"profile_text_color": "363636",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 29835,
+			"favorite_count": 64049,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "No alternative :( All other favorites like Opera also now use the chromium engine. https://t.co/IqQOEo2KzM"
+	},
+	{
 		"created_at": "Mon Jan 21 21:55:35 +0000 2019",
 		"id": 1087468776411390000,
 		"id_str": "1087468776411389954",
