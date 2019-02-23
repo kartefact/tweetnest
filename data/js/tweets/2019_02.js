a3c9083158757f7424859dc9d1df52c81e22f729
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-02-23 05:42:30 +0530

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,88 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Fri Feb 22 16:52:00 +0000 2019",
+		"id": 1098988789408555000,
+		"id_str": "1098988789408555008",
+		"full_text": "22:22 on 22/2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "Present is the reward",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1553,
+			"friends_count": 530,
+			"listed_count": 55,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10660,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 99246,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1550166431",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "22:22 on 22/2"
+	},
+	{
 		"created_at": "Thu Feb 21 16:42:40 +0000 2019",
 		"id": 1098624052724404200,
 		"id_str": "1098624052724404228",
@@ -523,7 +605,7 @@ Grailbird.data.tweets_2019_02 =
 		"created_at": "Sat Feb 16 04:32:49 +0000 2019",
 		"id": 1096628442437627900,
 		"id_str": "1096628442437627905",
-		"full_text": "Well, it *is* an operating system 🤷‍♂️ https://t.co/m5DEp8bZdH",
+		"full_text": "Well, it *is* an operating system 🤷♂️ https://t.co/m5DEp8bZdH",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -678,7 +760,7 @@ Grailbird.data.tweets_2019_02 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Well, it *is* an operating system 🤷‍♂️ https://t.co/m5DEp8bZdH"
+		"text": "Well, it *is* an operating system 🤷♂️ https://t.co/m5DEp8bZdH"
 	},
 	{
 		"created_at": "Sun Feb 17 16:35:10 +0000 2019",
