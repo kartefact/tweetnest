404edbb30ab02696271cc5a99c8ad2916b175432
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-04-02 05:42:30 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100691,
-	"created_at": "Mon Apr 01 00:12:36 +0000 2019",
+	"tweets": 100692,
+	"created_at": "Tue Apr 02 00:12:29 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_04.js
@@ -0,0 +1,198 @@
+Grailbird.data.tweets_2019_04 = 
+[
+	{
+		"created_at": "Mon Apr 01 05:28:17 +0000 2019",
+		"id": 1111099792753459200,
+		"id_str": "1112587466123501568",
+		"full_text": "“Meet the People Coding Our World” by @emjsmith https://t.co/jGtt3tTTDF https://t.co/zyfP9YSUyn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "emjsmith",
+					"name": "emily j. smith",
+					"id": 2197411,
+					"id_str": "2197411",
+					"indices": [
+						38,
+						47
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/jGtt3tTTDF",
+					"expanded_url": "https://link.medium.com/hiMl6NFJpV",
+					"display_url": "link.medium.com/hiMl6NFJpV",
+					"indices": [
+						48,
+						71
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1111099785707044900,
+					"id_str": "1111099785707044864",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/D2trXkUUwAAnKRg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2trXkUUwAAnKRg.jpg",
+					"url": "https://t.co/zyfP9YSUyn",
+					"display_url": "pic.twitter.com/zyfP9YSUyn",
+					"expanded_url": "https://twitter.com/xiaoma/status/1111099792753459202/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 619,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 619,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 561,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1111099785707044900,
+					"id_str": "1111099785707044864",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/D2trXkUUwAAnKRg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2trXkUUwAAnKRg.jpg",
+					"url": "https://t.co/zyfP9YSUyn",
+					"display_url": "pic.twitter.com/zyfP9YSUyn",
+					"expanded_url": "https://twitter.com/xiaoma/status/1111099792753459202/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 619,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 619,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 561,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1111099318985842700,
+		"in_reply_to_status_id_str": "1111099318985842688",
+		"in_reply_to_user_id": 10770012,
+		"in_reply_to_user_id_str": "10770012",
+		"in_reply_to_screen_name": "xiaoma",
+		"user": {
+			"id": 10770012,
+			"id_str": "10770012",
+			"name": "Xiao Ma",
+			"screen_name": "xiaoma",
+			"location": "San Francisco, CA",
+			"description": "Chief Architect @Medium. Serving Engineers. Teaching Machines. The ultimate goal of tech is to help us live better. Built @PatternInsight PhD @IllinoisCS UCSD",
+			"url": "https://t.co/DhQ0gICXHd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DhQ0gICXHd",
+							"expanded_url": "https://medium.com/@xiaoma",
+							"display_url": "medium.com/@xiaoma",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1866,
+			"friends_count": 1156,
+			"listed_count": 49,
+			"created_at": "Sat Dec 01 18:14:46 +0000 2007",
+			"favourites_count": 3210,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3413,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1823020654/image_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1823020654/image_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/10770012/1505884932",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 34,
+		"favorite_count": 94,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“Meet the People Coding Our World” by @emjsmith https://t.co/jGtt3tTTDF https://t.co/zyfP9YSUyn"
+	}
+]
