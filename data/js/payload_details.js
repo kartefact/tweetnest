745689aae54f2d84c48fa70b19a0ba4d0d4639c6
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-05-18 05:42:31 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100831,
-	"created_at": "Fri May 17 00:12:29 +0000 2019",
+	"tweets": 100832,
+	"created_at": "Sat May 18 00:12:30 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 17 11:58:04 +0000 2019",
+		"id": 1129355399251939300,
+		"id_str": "1129355399251939329",
+		"full_text": "@rajvj I use opera browser with free VPN. Works for everything I need.",
+		"truncated": false,
+		"display_text_range": [
+			7,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rajvj",
+					"name": "Vijay Raj",
+					"id": 17756787,
+					"id_str": "17756787",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1129350730953908200,
+		"in_reply_to_status_id_str": "1129350730953908225",
+		"in_reply_to_user_id": 17756787,
+		"in_reply_to_user_id_str": "17756787",
+		"in_reply_to_screen_name": "rajvj",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "Present is the reward",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1547,
+			"friends_count": 213,
+			"listed_count": 53,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10718,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 99399,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1550166431",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@rajvj I use opera browser with free VPN. Works for everything I need."
+	},
+	{
 		"created_at": "Thu May 16 11:32:33 +0000 2019",
 		"id": 1128407469808660500,
 		"id_str": "1128986591613968384",
@@ -579,7 +672,7 @@ Grailbird.data.tweets_2019_05 =
 			"name": "April.py",
 			"screen_name": "vogueandcode",
 			"location": "Los Angeles, CA",
-			"description": "Python developer 🐍 + 80s/90s pop culture aficionado ⚡️ I talk to computers for fun 👩🏾‍💻 + help build interactive experiences for @docsmsft 📝",
+			"description": "Python developer 🐍 + 80s/90s pop culture aficionado ⚡️ I talk to computers for fun 👩🏾💻 + help build interactive experiences for @docsmsft 📝",
 			"url": "https://t.co/M2e2cJYxMn",
 			"entities": {
 				"url": {
@@ -1166,7 +1259,7 @@ Grailbird.data.tweets_2019_05 =
 		"user": {
 			"id": 3502400774,
 			"id_str": "3502400774",
-			"name": "Tanisha | تنیشا 🏳️‍🌈",
+			"name": "Tanisha | تنیشا 🏳️🌈",
 			"screen_name": "slutsandgiggles",
 			"location": "",
 			"description": "Zoologist turned Development researcher | I tweet about sexual and mental health | Currently reading Ethical Slut | Bi/Poly | She/her",
