289ba815ede507da6928d476c8b2bdf8cd30953f
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-06-07 05:42:29 +0530

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100877,
-	"created_at": "Tue Jun 04 00:12:32 +0000 2019",
+	"tweets": 100879,
+	"created_at": "Fri Jun 07 00:12:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,403 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Thu Jun 06 06:48:28 +0000 2019",
+		"id": 1136463483434700800,
+		"id_str": "1136525244393373697",
+		"full_text": "We make rules for ourselves as we age. Some are healthy boundaries — others are born from fear. I’m working to learn the difference to avoid this “pen line prison”. https://t.co/0Zmesw8Aez",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1136463323631743000,
+					"id_str": "1136463323631742977",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1136463323631742977/pu/img/My69u_N-KwY5GfgH.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1136463323631742977/pu/img/My69u_N-KwY5GfgH.jpg",
+					"url": "https://t.co/0Zmesw8Aez",
+					"display_url": "pic.twitter.com/0Zmesw8Aez",
+					"expanded_url": "https://twitter.com/RachelTobac/status/1136463483434700800/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1136463323631743000,
+					"id_str": "1136463323631742977",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1136463323631742977/pu/img/My69u_N-KwY5GfgH.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1136463323631742977/pu/img/My69u_N-KwY5GfgH.jpg",
+					"url": "https://t.co/0Zmesw8Aez",
+					"display_url": "pic.twitter.com/0Zmesw8Aez",
+					"expanded_url": "https://twitter.com/RachelTobac/status/1136463483434700800/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 21762,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1136463323631742977/pu/vid/720x720/decp6p_fVeiN3vBd.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1136463323631742977/pu/vid/480x480/gFStGvbfoVFYHRLz.mp4?tag=10"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1136463323631742977/pu/vid/320x320/_C1WprTF6smecwcM.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1136463323631742977/pu/pl/yFSG1j9KhV17DesD.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3094974153,
+			"id_str": "3094974153",
+			"name": "Rachel Tobac",
+			"screen_name": "RachelTobac",
+			"location": "San Francisco and Pittsburgh",
+			"description": "CEO @SocialProofSec social engineering training/pentesting/speaking | 3X @DEFCON #SECTF 2nd place | Chair @WISPorg Board | Sr UX Research @coursehero | she/her",
+			"url": "https://t.co/JZnQ21bJAR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/JZnQ21bJAR",
+							"expanded_url": "https://www.SocialProofSecurity.com/",
+							"display_url": "SocialProofSecurity.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 28072,
+			"friends_count": 5375,
+			"listed_count": 495,
+			"created_at": "Wed Mar 18 17:30:53 +0000 2015",
+			"favourites_count": 30710,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10826,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/962413102296780801/E3xV4Pen_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/962413102296780801/E3xV4Pen_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3094974153/1518283616",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 405,
+		"favorite_count": 1016,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We make rules for ourselves as we age. Some are healthy boundaries — others are born from fear. I’m working to learn the difference to avoid this “pen line prison”. https://t.co/0Zmesw8Aez"
+	},
+	{
+		"created_at": "Thu Jun 06 06:20:21 +0000 2019",
+		"id": 1136087637914247200,
+		"id_str": "1136518168866803712",
+		"full_text": "Y'all , watch this baby have a full damn convo with his daddy 😂😂😂 https://t.co/gEbtJZ6xuP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			65
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1136087374071586800,
+					"id_str": "1136087374071586816",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1136087374071586816/pu/img/UWVDCjDg9dZVHmX4.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1136087374071586816/pu/img/UWVDCjDg9dZVHmX4.jpg",
+					"url": "https://t.co/gEbtJZ6xuP",
+					"display_url": "pic.twitter.com/gEbtJZ6xuP",
+					"expanded_url": "https://twitter.com/_11Remember_/status/1136087637914247170/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1136087374071586800,
+					"id_str": "1136087374071586816",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1136087374071586816/pu/img/UWVDCjDg9dZVHmX4.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1136087374071586816/pu/img/UWVDCjDg9dZVHmX4.jpg",
+					"url": "https://t.co/gEbtJZ6xuP",
+					"display_url": "pic.twitter.com/gEbtJZ6xuP",
+					"expanded_url": "https://twitter.com/_11Remember_/status/1136087637914247170/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 78093,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1136087374071586816/pu/vid/360x640/E8k67ABS8SnRdDVX.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1136087374071586816/pu/pl/daizD-k87fc6WyUO.m3u8?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1136087374071586816/pu/vid/320x568/VPKVt8FawUE4ae1r.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1136087374071586816/pu/vid/720x1280/LlccgKfuYycGrS1y.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1090298017314455600,
+			"id_str": "1090298017314455552",
+			"name": "Devin Johnson",
+			"screen_name": "_11Remember_",
+			"location": "Minding My Business ",
+			"description": "Old account got suspended so I had to make a new one 🤦🏾‍♂️ AAMU21 🐾",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8493,
+			"friends_count": 128,
+			"listed_count": 8,
+			"created_at": "Tue Jan 29 17:17:58 +0000 2019",
+			"favourites_count": 68,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 379,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1136371058141831170/KCqwAa_t_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1136371058141831170/KCqwAa_t_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1090298017314455552/1553525727",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 940944,
+		"favorite_count": 3185927,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Y'all , watch this baby have a full damn convo with his daddy 😂😂😂 https://t.co/gEbtJZ6xuP"
+	},
+	{
 		"created_at": "Mon Jun 03 12:43:42 +0000 2019",
 		"id": 1134560492452810800,
 		"id_str": "1135527480050065408",
