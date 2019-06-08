8d731a0a1cb418ce0f1e9a7a8df0abdbc8b4d904
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-06-08 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100879,
-	"created_at": "Fri Jun 07 00:12:28 +0000 2019",
+	"tweets": 100880,
+	"created_at": "Sat Jun 08 00:12:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Fri Jun 07 14:40:08 +0000 2019",
+		"id": 1137006332588855300,
+		"id_str": "1137006332588855296",
+		"full_text": "@iBakasura Happy birthday!",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			26
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "iBakasura",
+					"name": "Tj",
+					"id": 115513801,
+					"id_str": "115513801",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1136819397044006900,
+		"in_reply_to_status_id_str": "1136819397044006912",
+		"in_reply_to_user_id": 115513801,
+		"in_reply_to_user_id_str": "115513801",
+		"in_reply_to_screen_name": "iBakasura",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "Present is the reward",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1535,
+			"friends_count": 213,
+			"listed_count": 53,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10751,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 99447,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1550166431",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@iBakasura Happy birthday!"
+	},
+	{
 		"created_at": "Thu Jun 06 06:48:28 +0000 2019",
 		"id": 1136463483434700800,
 		"id_str": "1136525244393373697",
@@ -342,7 +435,7 @@ Grailbird.data.tweets_2019_06 =
 			"name": "Devin Johnson",
 			"screen_name": "_11Remember_",
 			"location": "Minding My Business ",
-			"description": "Old account got suspended so I had to make a new one 🤦🏾‍♂️ AAMU21 🐾",
+			"description": "Old account got suspended so I had to make a new one 🤦🏾♂️ AAMU21 🐾",
 			"url": null,
 			"entities": {
 				"description": {
