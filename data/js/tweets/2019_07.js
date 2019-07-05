adbf8988ad1f2154b7661b91796987f94b9c5048
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-07-05 05:42:30 +0530

    Added 1 tweets

--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 04 18:45:32 +0000 2019",
+		"id": 1146456110917914600,
+		"id_str": "1146852561380499456",
+		"full_text": "Be a part of the computing revolution and get your copy of Windows 1.0 today! https://t.co/VJzgYnfEvB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			77
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1146456065929863200,
+					"id_str": "1146456065929863169",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1146456065929863169/img/k7cY-kebHg-ZV1T6.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1146456065929863169/img/k7cY-kebHg-ZV1T6.jpg",
+					"url": "https://t.co/VJzgYnfEvB",
+					"display_url": "pic.twitter.com/VJzgYnfEvB",
+					"expanded_url": "https://twitter.com/Windows/status/1146456110917914624/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1146456065929863200,
+					"id_str": "1146456065929863169",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1146456065929863169/img/k7cY-kebHg-ZV1T6.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1146456065929863169/img/k7cY-kebHg-ZV1T6.jpg",
+					"url": "https://t.co/VJzgYnfEvB",
+					"display_url": "pic.twitter.com/VJzgYnfEvB",
+					"expanded_url": "https://twitter.com/Windows/status/1146456110917914624/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 19375,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1146456065929863169/vid/720x720/liFrPfnN9SggK8Kg.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1146456065929863169/vid/480x480/C0arrFek8ERaR4ij.mp4?tag=13"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1146456065929863169/vid/320x320/Ff_wx1kjcbEMXxql.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1146456065929863169/pl/3W4ZlYkithjSESn8.m3u8?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://prod1.sprinklr.com\" rel=\"nofollow\">Sprinklr Publishing</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15670515,
+			"id_str": "15670515",
+			"name": "Windows",
+			"screen_name": "Windows",
+			"location": "Redmond, Washington",
+			"description": "Windows news, product info & global stories of people using Windows technology to #DoGreatThings. For support tweet @MicrosoftHelps",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6246889,
+			"friends_count": 1733,
+			"listed_count": 19518,
+			"created_at": "Thu Jul 31 03:43:16 +0000 2008",
+			"favourites_count": 859,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 318081,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "00BCF2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1145728560994238465/YmBM8qkO_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1145728560994238465/YmBM8qkO_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15670515/1561997966",
+			"profile_link_color": "00BCF2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F9FFE5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2018,
+		"favorite_count": 9058,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Be a part of the computing revolution and get your copy of Windows 1.0 today! https://t.co/VJzgYnfEvB"
+	},
+	{
 		"created_at": "Tue Jul 02 02:59:43 +0000 2019",
 		"id": 1145889762018021400,
 		"id_str": "1145889762018021381",
