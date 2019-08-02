7cbc5feee89aed5d12e75968c65e650f110e4a0f
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-08-02 05:42:30 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100907,
-	"created_at": "Mon Jul 29 00:12:28 +0000 2019",
+	"tweets": 100908,
+	"created_at": "Fri Aug 02 00:12:29 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_08.js
@@ -0,0 +1,208 @@
+Grailbird.data.tweets_2019_08 = 
+[
+	{
+		"created_at": "Thu Aug 01 14:02:22 +0000 2019",
+		"id": 1156918640379347000,
+		"id_str": "1156928160849510407",
+		"full_text": "Intel Launches the First 10th Gen Ice Lake Processors - by @mehedih_ - https://t.co/y5Csz2cUlO https://t.co/3htHiE0a85",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mehedih_",
+					"name": "mehedi",
+					"id": 2337242269,
+					"id_str": "2337242269",
+					"indices": [
+						59,
+						68
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/y5Csz2cUlO",
+					"expanded_url": "https://www.thurrott.com/hardware/211441/intel-launches-the-first-10th-gen-ice-lake-processors",
+					"display_url": "thurrott.com/hardware/21144…",
+					"indices": [
+						71,
+						94
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1156918638504554500,
+					"id_str": "1156918638504554496",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EA4zX0IXsAAEC2H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA4zX0IXsAAEC2H.jpg",
+					"url": "https://t.co/3htHiE0a85",
+					"display_url": "pic.twitter.com/3htHiE0a85",
+					"expanded_url": "https://twitter.com/thurrott/status/1156918640379346944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1156918638504554500,
+					"id_str": "1156918638504554496",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EA4zX0IXsAAEC2H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA4zX0IXsAAEC2H.jpg",
+					"url": "https://t.co/3htHiE0a85",
+					"display_url": "pic.twitter.com/3htHiE0a85",
+					"expanded_url": "https://twitter.com/thurrott/status/1156918640379346944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.thurrott.com\" rel=\"nofollow\">Thurrott Dot Com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14964767,
+			"id_str": "14964767",
+			"name": "Paul Thurrott",
+			"screen_name": "thurrott",
+			"location": "Emmaus, PA",
+			"description": "Paul Thurrott is an award-winning technology journalist and blogger and the author of over 25 books. He is the majordomo at http://t.co/DpuqUugruE.",
+			"url": "http://t.co/HTMQCSc0Np",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/HTMQCSc0Np",
+							"expanded_url": "http://www.thurrott.com",
+							"display_url": "thurrott.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "http://t.co/DpuqUugruE",
+							"expanded_url": "http://Thurrott.com",
+							"display_url": "Thurrott.com",
+							"indices": [
+								124,
+								146
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 114659,
+			"friends_count": 150,
+			"listed_count": 6280,
+			"created_at": "Sat May 31 17:47:07 +0000 2008",
+			"favourites_count": 7787,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 112503,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/819277710988025856/0Nma3VWJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/819277710988025856/0Nma3VWJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14964767/1444060559",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Intel Launches the First 10th Gen Ice Lake Processors - by @mehedih_ - https://t.co/y5Csz2cUlO https://t.co/3htHiE0a85"
+	}
+]
