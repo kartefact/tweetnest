78ced644c7b6043b4e65c0dcc53108062d3f5bf4
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-08-17 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100911,
-	"created_at": "Thu Aug 15 00:12:28 +0000 2019",
+	"tweets": 100912,
+	"created_at": "Sat Aug 17 00:12:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 16 20:47:35 +0000 2019",
+		"id": 1162455068676886500,
+		"id_str": "1162465953784840192",
+		"full_text": "Windows 10's Latest 20H1 Build Introduces a Number of New Features - by @mehedih_ - https://t.co/iCWCbaYL5k https://t.co/XtcZ7CIKir",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			107
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mehedih_",
+					"name": "mehedi",
+					"id": 2337242269,
+					"id_str": "2337242269",
+					"indices": [
+						72,
+						81
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/iCWCbaYL5k",
+					"expanded_url": "https://www.thurrott.com/windows/windows-10/212321/windows-10s-latest-20h1-build-introduces-a-number-of-new-features",
+					"display_url": "thurrott.com/windows/window…",
+					"indices": [
+						84,
+						107
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1162455066860761000,
+					"id_str": "1162455066860761088",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/ECHeuW-WsAANWAO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECHeuW-WsAANWAO.jpg",
+					"url": "https://t.co/XtcZ7CIKir",
+					"display_url": "pic.twitter.com/XtcZ7CIKir",
+					"expanded_url": "https://twitter.com/thurrott/status/1162455068676886528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1162455066860761000,
+					"id_str": "1162455066860761088",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/ECHeuW-WsAANWAO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECHeuW-WsAANWAO.jpg",
+					"url": "https://t.co/XtcZ7CIKir",
+					"display_url": "pic.twitter.com/XtcZ7CIKir",
+					"expanded_url": "https://twitter.com/thurrott/status/1162455068676886528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.thurrott.com\" rel=\"nofollow\">Thurrott Dot Com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14964767,
+			"id_str": "14964767",
+			"name": "Paul Thurrott",
+			"screen_name": "thurrott",
+			"location": "Emmaus, PA",
+			"description": "Paul Thurrott is an award-winning technology journalist and blogger and the author of over 25 books. He is the majordomo at http://t.co/DpuqUugruE.",
+			"url": "http://t.co/HTMQCSc0Np",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/HTMQCSc0Np",
+							"expanded_url": "http://www.thurrott.com",
+							"display_url": "thurrott.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "http://t.co/DpuqUugruE",
+							"expanded_url": "http://Thurrott.com",
+							"display_url": "Thurrott.com",
+							"indices": [
+								124,
+								146
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 114605,
+			"friends_count": 150,
+			"listed_count": 6279,
+			"created_at": "Sat May 31 17:47:07 +0000 2008",
+			"favourites_count": 7836,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 112668,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/819277710988025856/0Nma3VWJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/819277710988025856/0Nma3VWJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14964767/1444060559",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Windows 10's Latest 20H1 Build Introduces a Number of New Features - by @mehedih_ - https://t.co/iCWCbaYL5k https://t.co/XtcZ7CIKir"
+	},
+	{
 		"created_at": "Wed Aug 14 18:30:34 +0000 2019",
 		"id": 1161706694537146400,
 		"id_str": "1161706694537146368",
