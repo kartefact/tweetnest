d831a79cd5eb0d56856a21cc436bae9c687bbe78
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-09-07 05:42:30 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100914,
-	"created_at": "Tue Sep 03 00:12:29 +0000 2019",
+	"tweets": 100915,
+	"created_at": "Sat Sep 07 00:12:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Fri Sep 06 15:43:38 +0000 2019",
+		"id": 1169655249943949300,
+		"id_str": "1169999606488264710",
+		"full_text": "5th September, Spinal Cord Injury Day\nI had never heard of a spinal cord injury till I got mine.... https://t.co/o8VjZsIxHX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1169655212472057900,
+					"id_str": "1169655212472057856",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/EDtzN_kUwAAnYUl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDtzN_kUwAAnYUl.jpg",
+					"url": "https://t.co/o8VjZsIxHX",
+					"display_url": "pic.twitter.com/o8VjZsIxHX",
+					"expanded_url": "https://twitter.com/mrunmaiy/status/1169655249943949312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1053,
+							"h": 1675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 427,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 754,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1169655212472057900,
+					"id_str": "1169655212472057856",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/EDtzN_kUwAAnYUl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDtzN_kUwAAnYUl.jpg",
+					"url": "https://t.co/o8VjZsIxHX",
+					"display_url": "pic.twitter.com/o8VjZsIxHX",
+					"expanded_url": "https://twitter.com/mrunmaiy/status/1169655249943949312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1053,
+							"h": 1675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 427,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 754,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1169655238195699700,
+					"id_str": "1169655238195699712",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/EDtzPfZUYAA_1cX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDtzPfZUYAA_1cX.jpg",
+					"url": "https://t.co/o8VjZsIxHX",
+					"display_url": "pic.twitter.com/o8VjZsIxHX",
+					"expanded_url": "https://twitter.com/mrunmaiy/status/1169655249943949312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 20658808,
+			"id_str": "20658808",
+			"name": "Mrunmaiy",
+			"screen_name": "mrunmaiy",
+			"location": "Bengaluru, India",
+			"description": "Passionate about technology & travel | Quadriplegic #SCI Spinal Cord Injury | #WheelchairWanderer | Managing PR for Devices @AmazonIN | @AmazonEchoIndia",
+			"url": "https://t.co/ylUEh2MHY5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ylUEh2MHY5",
+							"expanded_url": "https://mrunmaiy.com",
+							"display_url": "mrunmaiy.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2515,
+			"friends_count": 1359,
+			"listed_count": 90,
+			"created_at": "Thu Feb 12 06:36:53 +0000 2009",
+			"favourites_count": 1657,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8368,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1153276761997807622/dTnLY_XP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1153276761997807622/dTnLY_XP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/20658808/1404191966",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 37,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "5th September, Spinal Cord Injury Day\nI had never heard of a spinal cord injury till I got mine.... https://t.co/o8VjZsIxHX"
+	},
+	{
 		"created_at": "Mon Sep 02 10:38:04 +0000 2019",
 		"id": 1168473155649687600,
 		"id_str": "1168473155649687552",
