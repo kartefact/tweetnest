0e13df357d63e5c03be4216aa19c6c6cce7d0b65
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-09-14 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100916,
-	"created_at": "Fri Sep 13 00:12:29 +0000 2019",
+	"tweets": 100917,
+	"created_at": "Sat Sep 14 00:12:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,279 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Fri Sep 13 16:17:41 +0000 2019",
+		"id": 1172490305733480400,
+		"id_str": "1172544890804113409",
+		"full_text": "Reminded me of Shin Chan 😅 https://t.co/EqMO9vGnGe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			50
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/EqMO9vGnGe",
+					"expanded_url": "https://twitter.com/SarahPinborough/status/1171861951275831296?s=19",
+					"display_url": "twitter.com/SarahPinboroug…",
+					"indices": [
+						27,
+						50
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1685849366,
+			"id_str": "1685849366",
+			"name": "Mud n Sky",
+			"screen_name": "mudnsky",
+			"location": "India",
+			"description": "''This man has conquered the world! What have you done?'' ''I have conquered the need to conquer the world.'' - Pressfield",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 261,
+			"friends_count": 208,
+			"listed_count": 35,
+			"created_at": "Tue Aug 20 13:48:52 +0000 2013",
+			"favourites_count": 5535,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33305,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1092065435359399937/_2hAYAUz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1092065435359399937/_2hAYAUz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1685849366/1507642702",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1171861951275831300,
+		"quoted_status_id_str": "1171861951275831296",
+		"quoted_status_permalink": {
+			"url": "https://t.co/EqMO9vGnGe",
+			"expanded": "https://twitter.com/SarahPinborough/status/1171861951275831296?s=19",
+			"display": "twitter.com/SarahPinboroug…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Sep 11 19:03:55 +0000 2019",
+			"id": 1171861951275831300,
+			"id_str": "1171861951275831296",
+			"full_text": "Brilliant. https://t.co/yxJNYl2GSr",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				10
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1171861930908246000,
+						"id_str": "1171861930908246021",
+						"indices": [
+							11,
+							34
+						],
+						"media_url": "http://pbs.twimg.com/media/EENKN6WW4AU_xZM.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EENKN6WW4AU_xZM.jpg",
+						"url": "https://t.co/yxJNYl2GSr",
+						"display_url": "pic.twitter.com/yxJNYl2GSr",
+						"expanded_url": "https://twitter.com/SarahPinborough/status/1171861951275831296/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 720,
+								"h": 960,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 960,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1171861930908246000,
+						"id_str": "1171861930908246021",
+						"indices": [
+							11,
+							34
+						],
+						"media_url": "http://pbs.twimg.com/media/EENKN6WW4AU_xZM.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EENKN6WW4AU_xZM.jpg",
+						"url": "https://t.co/yxJNYl2GSr",
+						"display_url": "pic.twitter.com/yxJNYl2GSr",
+						"expanded_url": "https://twitter.com/SarahPinborough/status/1171861951275831296/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 720,
+								"h": 960,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 960,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 300246481,
+				"id_str": "300246481",
+				"name": "Sarah Pinborough (is working but will be back)",
+				"screen_name": "SarahPinborough",
+				"location": "Somewhere I shouldn't be.",
+				"description": "Scribbler. Sunday Times #1, and NYT Bestselling author of 'Behind Her Eyes' (On Netflix next year) and ‘Cross Her Heart’. Plus a lot of less successful books;-)",
+				"url": "https://t.co/uDJM1SwcES",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/uDJM1SwcES",
+								"expanded_url": "http://www.sarahpinborough.com",
+								"display_url": "sarahpinborough.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 17482,
+				"friends_count": 1733,
+				"listed_count": 421,
+				"created_at": "Tue May 17 13:04:51 +0000 2011",
+				"favourites_count": 20605,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 98830,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1169334451928731654/URKRJvfv_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1169334451928731654/URKRJvfv_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/300246481/1550678302",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 19922,
+			"favorite_count": 55598,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Reminded me of Shin Chan 😅 https://t.co/EqMO9vGnGe"
+	},
+	{
 		"created_at": "Thu Sep 12 15:35:03 +0000 2019",
 		"id": 1172171775431458800,
 		"id_str": "1172171775431458816",
