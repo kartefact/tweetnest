6251acacfd78968132610b1cfbee327e9000387b
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-11-02 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100944,
-	"created_at": "Fri Nov 01 00:12:28 +0000 2019",
+	"tweets": 100945,
+	"created_at": "Sat Nov 02 00:12:28 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,116 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Fri Nov 01 10:21:19 +0000 2019",
+		"id": 1190212212456951800,
+		"id_str": "1190212212456951809",
+		"full_text": "@zenrainman @rajagopalanv I just read that tweet in the voice of \"Mad-eye Moody\" from Harry Potter. #ConstantVigilance",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			118
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ConstantVigilance",
+					"indices": [
+						100,
+						118
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "zenrainman",
+					"name": "zenrainman",
+					"id": 18285948,
+					"id_str": "18285948",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "rajagopalanv",
+					"name": "Rajagopalan V",
+					"id": 92552176,
+					"id_str": "92552176",
+					"indices": [
+						12,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1190194967982923800,
+		"in_reply_to_status_id_str": "1190194967982923776",
+		"in_reply_to_user_id": 18285948,
+		"in_reply_to_user_id_str": "18285948",
+		"in_reply_to_screen_name": "zenrainman",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1525,
+			"friends_count": 216,
+			"listed_count": 52,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10739,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 99412,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@zenrainman @rajagopalanv I just read that tweet in the voice of \"Mad-eye Moody\" from Harry Potter. #ConstantVigilance"
+	}
+]
