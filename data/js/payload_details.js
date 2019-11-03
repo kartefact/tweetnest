b823d920cca611d7b7c12694093acb05b137f8c5
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-11-03 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100945,
-	"created_at": "Sat Nov 02 00:12:28 +0000 2019",
+	"tweets": 100946,
+	"created_at": "Sun Nov 03 00:12:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 02 02:28:40 +0000 2019",
+		"id": 1190059381880172500,
+		"id_str": "1190455653501788160",
+		"full_text": "This baby deer is so carefree and happy. I wanna be this happy 😭❤ https://t.co/sAYJCpM7r7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			65
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1190059354529112000,
+					"id_str": "1190059354529112065",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1190059354529112065/pu/img/4w7aC0K8JdAw_7Pk.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1190059354529112065/pu/img/4w7aC0K8JdAw_7Pk.jpg",
+					"url": "https://t.co/sAYJCpM7r7",
+					"display_url": "pic.twitter.com/sAYJCpM7r7",
+					"expanded_url": "https://twitter.com/_SJPeace_/status/1190059381880172546/video/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1190059354529112000,
+					"id_str": "1190059354529112065",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1190059354529112065/pu/img/4w7aC0K8JdAw_7Pk.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1190059354529112065/pu/img/4w7aC0K8JdAw_7Pk.jpg",
+					"url": "https://t.co/sAYJCpM7r7",
+					"display_url": "pic.twitter.com/sAYJCpM7r7",
+					"expanded_url": "https://twitter.com/_SJPeace_/status/1190059381880172546/video/1",
+					"type": "video",
+					"sizes": {
+						"small": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 13033,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1190059354529112065/pu/vid/480x480/fEZ3wcBm-g9D5CyF.mp4?tag=10"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1190059354529112065/pu/vid/320x320/MBfbcQCHf7cvVFS4.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1190059354529112065/pu/pl/d0oZ_TtIOmSoY7TE.m3u8?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 716432228,
+			"id_str": "716432228",
+			"name": "StanceGrounded",
+			"screen_name": "_SJPeace_",
+			"location": "Detroit, MI",
+			"description": "Human/Civil Rights Activist- Muslim, American by birth, Pakistani. Educated man of color - changing the world one friend at a time. ✊🏽✊🏾✊🏿 #Resist #BLM",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 301426,
+			"friends_count": 45951,
+			"listed_count": 1195,
+			"created_at": "Wed Jul 25 17:22:39 +0000 2012",
+			"favourites_count": 12701,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15604,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152128665398009857/I8-hm2NX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152128665398009857/I8-hm2NX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/716432228/1548989721",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4448,
+		"favorite_count": 32024,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This baby deer is so carefree and happy. I wanna be this happy 😭❤ https://t.co/sAYJCpM7r7"
+	},
+	{
 		"created_at": "Fri Nov 01 10:21:19 +0000 2019",
 		"id": 1190212212456951800,
 		"id_str": "1190212212456951809",
