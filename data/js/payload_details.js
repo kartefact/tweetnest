45ee0441791265915e88f9a3fe0ba4ec5635f3bd
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-11-17 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100953,
-	"created_at": "Tue Nov 12 00:12:28 +0000 2019",
+	"tweets": 100954,
+	"created_at": "Sun Nov 17 00:12:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,195 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 16 20:10:53 +0000 2019",
+		"id": 1195796401327755300,
+		"id_str": "1195796401327755264",
+		"full_text": "@edbidangi Congrats, buddy. Welcome to the club. 😀 https://t.co/kgBeMtwP48",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			50
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "edbidangi",
+					"name": "Vijesh",
+					"id": 258012996,
+					"id_str": "258012996",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1195796388937756700,
+					"id_str": "1195796388937756672",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EJhSeupUcAA2LXR.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EJhSeupUcAA2LXR.jpg",
+					"url": "https://t.co/kgBeMtwP48",
+					"display_url": "pic.twitter.com/kgBeMtwP48",
+					"expanded_url": "https://twitter.com/kartefact/status/1195796401327755264/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 220,
+							"h": 136,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 220,
+							"h": 136,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 136,
+							"h": 136,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 220,
+							"h": 136,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1195796388937756700,
+					"id_str": "1195796388937756672",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EJhSeupUcAA2LXR.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EJhSeupUcAA2LXR.jpg",
+					"url": "https://t.co/kgBeMtwP48",
+					"display_url": "pic.twitter.com/kgBeMtwP48",
+					"expanded_url": "https://twitter.com/kartefact/status/1195796401327755264/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 220,
+							"h": 136,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 220,
+							"h": 136,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 136,
+							"h": 136,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 220,
+							"h": 136,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							55,
+							34
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EJhSeupUcAA2LXR.mp4"
+							}
+						]
+					},
+					"features": {}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1195648936532304000,
+		"in_reply_to_status_id_str": "1195648936532303872",
+		"in_reply_to_user_id": 258012996,
+		"in_reply_to_user_id_str": "258012996",
+		"in_reply_to_screen_name": "edbidangi",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1523,
+			"friends_count": 216,
+			"listed_count": 53,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10727,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 99411,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@edbidangi Congrats, buddy. Welcome to the club. 😀 https://t.co/kgBeMtwP48"
+	},
+	{
 		"created_at": "Mon Nov 11 16:53:05 +0000 2019",
 		"id": 1193934682116636700,
 		"id_str": "1193934682116636676",
