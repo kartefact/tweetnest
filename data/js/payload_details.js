6fef57bc6fec848145ff81e09330a024d346f3a6
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-11-30 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100955,
-	"created_at": "Thu Nov 28 00:12:29 +0000 2019",
+	"tweets": 100956,
+	"created_at": "Sat Nov 30 00:12:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 29 08:00:19 +0000 2019",
+		"id": 1200067634860937200,
+		"id_str": "1200323588508667906",
+		"full_text": "Heard on @Redfmbengaluru this morning about @blrcitytraffic traffic cop mannequins being deployed across Bangalore as deterrents to rule breakers, and guess what I spotted this evening? https://t.co/au2HrhY3M3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			185
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Redfmbengaluru",
+					"name": "93.5 RED FM, BLR",
+					"id": 975846859,
+					"id_str": "975846859",
+					"indices": [
+						9,
+						24
+					]
+				},
+				{
+					"screen_name": "blrcitytraffic",
+					"name": "BengaluruTrafficPolice",
+					"id": 1851545900,
+					"id_str": "1851545900",
+					"indices": [
+						44,
+						59
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1200067614195601400,
+					"id_str": "1200067614195601408",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/EKd_IwqUUAA5I2d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKd_IwqUUAA5I2d.jpg",
+					"url": "https://t.co/au2HrhY3M3",
+					"display_url": "pic.twitter.com/au2HrhY3M3",
+					"expanded_url": "https://twitter.com/2shar/status/1200067634860937222/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1200067614195601400,
+					"id_str": "1200067614195601408",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/EKd_IwqUUAA5I2d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKd_IwqUUAA5I2d.jpg",
+					"url": "https://t.co/au2HrhY3M3",
+					"display_url": "pic.twitter.com/au2HrhY3M3",
+					"expanded_url": "https://twitter.com/2shar/status/1200067634860937222/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14315601,
+			"id_str": "14315601",
+			"name": "Tushar Kanwar",
+			"screen_name": "2shar",
+			"location": "Bangalore",
+			"description": "Technology Columnist - Huffington Post @HuffPostIndia, Business World @BWBusinessworld, The Telegraph @t2telegraph. Opinions mine, obviously",
+			"url": "https://t.co/uH8VNwQeao",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uH8VNwQeao",
+							"expanded_url": "http://about.me/tushar.kanwar",
+							"display_url": "about.me/tushar.kanwar",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8264,
+			"friends_count": 858,
+			"listed_count": 206,
+			"created_at": "Sun Apr 06 14:25:59 +0000 2008",
+			"favourites_count": 2031,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 35479,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/652414794452963328/uVttKUau_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/652414794452963328/uVttKUau_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14315601/1398219585",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Heard on @Redfmbengaluru this morning about @blrcitytraffic traffic cop mannequins being deployed across Bangalore as deterrents to rule breakers, and guess what I spotted this evening? https://t.co/au2HrhY3M3"
+	},
+	{
 		"created_at": "Wed Nov 27 12:44:19 +0000 2019",
 		"id": 1199377654891368400,
 		"id_str": "1199670283516272642",
