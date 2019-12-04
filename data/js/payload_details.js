11de2965063517e95c0d78b61ef9c4338d742b83
Karthikeyan Ravikumar <kartefact@gmail.com>
2019-12-04 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 100957,
-	"created_at": "Mon Dec 02 00:12:28 +0000 2019",
+	"tweets": 100958,
+	"created_at": "Wed Dec 04 00:12:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Tue Dec 03 19:36:19 +0000 2019",
+		"id": 1201310009553850400,
+		"id_str": "1201948296777854976",
+		"full_text": "Bond is back. The first trailer for #NoTimeToDie arrives this Wednesday #Bond25 #BondJamesBond https://t.co/ThIEdXn82N",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NoTimeToDie",
+					"indices": [
+						36,
+						48
+					]
+				},
+				{
+					"text": "Bond25",
+					"indices": [
+						72,
+						79
+					]
+				},
+				{
+					"text": "BondJamesBond",
+					"indices": [
+						80,
+						94
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1200455055629070300,
+					"id_str": "1200455055629070336",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EKjfqaVX0AAY3Cy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKjfqaVX0AAY3Cy.jpg",
+					"url": "https://t.co/ThIEdXn82N",
+					"display_url": "pic.twitter.com/ThIEdXn82N",
+					"expanded_url": "https://twitter.com/007/status/1201310009553850368/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1200455055629070300,
+					"id_str": "1200455055629070336",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EKjfqaVX0AAY3Cy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKjfqaVX0AAY3Cy.jpg",
+					"url": "https://t.co/ThIEdXn82N",
+					"display_url": "pic.twitter.com/ThIEdXn82N",
+					"expanded_url": "https://twitter.com/007/status/1201310009553850368/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 14000,
+						"variants": [
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1200455055629070336/vid/480x270/C2eHgkeHuL2tLacA.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1200455055629070336/pl/0hlLlH6IPindkwOb.m3u8?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1200455055629070336/vid/640x360/uO6s9BbKYdvWPkoK.mp4?tag=13"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1200455055629070336/vid/1280x720/gbuF-WHhEZvpbKPz.mp4?tag=13"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 389229444,
+			"id_str": "389229444",
+			"name": "James Bond",
+			"screen_name": "007",
+			"location": "",
+			"description": "The official James Bond Twitter account. NO TIME TO DIE, gun barrel logo and related James Bond Indicia © Danjaq and MGM.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 514562,
+			"friends_count": 0,
+			"listed_count": 2551,
+			"created_at": "Wed Oct 12 02:17:35 +0000 2011",
+			"favourites_count": 27,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4475,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/876771373619843072/loRucdAt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/876771373619843072/loRucdAt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/389229444/1566467234",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 21920,
+		"favorite_count": 61992,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Bond is back. The first trailer for #NoTimeToDie arrives this Wednesday #Bond25 #BondJamesBond https://t.co/ThIEdXn82N"
+	},
+	{
 		"created_at": "Sun Dec 01 07:20:06 +0000 2019",
 		"id": 1199600883455017000,
 		"id_str": "1201038243292041216",
