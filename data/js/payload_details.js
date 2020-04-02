542e52512ad588d6b6c95e4c360699bb2d39f803
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-04-02 05:42:30 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101061,
-	"created_at": "Wed Apr 01 00:12:30 +0000 2020",
+	"tweets": 101062,
+	"created_at": "Thu Apr 02 00:12:29 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_04.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Wed Apr 01 13:09:23 +0000 2020",
+		"id": 1245329557399285800,
+		"id_str": "1245337464333463553",
+		"full_text": "Location Intelligence and &amp; Hyperlocal Data is very critical in these times. If anyone has COVID related data, please get in touch. Would be happy to put it out on the platform free of cost. RT for karma!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			208
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1245329555868356600,
+		"in_reply_to_status_id_str": "1245329555868356608",
+		"in_reply_to_user_id": 985169696994586600,
+		"in_reply_to_user_id_str": "985169696994586627",
+		"in_reply_to_screen_name": "aditi1002",
+		"user": {
+			"id": 985169696994586600,
+			"id_str": "985169696994586627",
+			"name": "Aditi Sinha",
+			"screen_name": "aditi1002",
+			"location": "Bengaluru, India",
+			"description": "Co-founder @Localeai. Making location data accessible and interactive for businesses of all kinds. @bitspilaniindia. Love jazz. And wine. And cheese.",
+			"url": "https://t.co/K3RiEle1Ux",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/K3RiEle1Ux",
+							"expanded_url": "https://locale.ai/",
+							"display_url": "locale.ai",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 284,
+			"friends_count": 1700,
+			"listed_count": 4,
+			"created_at": "Sat Apr 14 14:55:34 +0000 2018",
+			"favourites_count": 1219,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1048068670235107328/SQL9SfZY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1048068670235107328/SQL9SfZY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/985169696994586627/1545670216",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Location Intelligence and &amp; Hyperlocal Data is very critical in these times. If anyone has COVID related data, please get in touch. Would be happy to put it out on the platform free of cost. RT for karma!"
+	}
+]
