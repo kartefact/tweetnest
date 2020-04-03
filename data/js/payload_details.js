84d5a47246d5d51a7907037f75b899f7328d20ce
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-04-03 05:42:31 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101062,
-	"created_at": "Thu Apr 02 00:12:29 +0000 2020",
+	"tweets": 101063,
+	"created_at": "Fri Apr 03 00:12:30 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 02 17:13:34 +0000 2020",
+		"id": 1245560877547319300,
+		"id_str": "1245761303987146752",
+		"full_text": "@shl @gumroad https://t.co/BfxOHovXnR",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "shl",
+					"name": "Sahil Lavingia",
+					"id": 16347964,
+					"id_str": "16347964",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"screen_name": "gumroad",
+					"name": "Gumroad",
+					"id": 276271004,
+					"id_str": "276271004",
+					"indices": [
+						5,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245560839416864800,
+					"id_str": "1245560839416864769",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/EUke-_OUYAE1nMo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUke-_OUYAE1nMo.jpg",
+					"url": "https://t.co/BfxOHovXnR",
+					"display_url": "pic.twitter.com/BfxOHovXnR",
+					"expanded_url": "https://twitter.com/neilwithdata/status/1245560877547319301/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 563,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 563,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245560839416864800,
+					"id_str": "1245560839416864769",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/EUke-_OUYAE1nMo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUke-_OUYAE1nMo.jpg",
+					"url": "https://t.co/BfxOHovXnR",
+					"display_url": "pic.twitter.com/BfxOHovXnR",
+					"expanded_url": "https://twitter.com/neilwithdata/status/1245560877547319301/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 563,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 563,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1245550085250797600,
+		"in_reply_to_status_id_str": "1245550085250797568",
+		"in_reply_to_user_id": 16347964,
+		"in_reply_to_user_id_str": "16347964",
+		"in_reply_to_screen_name": "shl",
+		"user": {
+			"id": 14262889,
+			"id_str": "14262889",
+			"name": "Neil Sainsbury",
+			"screen_name": "neilwithdata",
+			"location": "Australia",
+			"description": "Building apps for Slack and Teams. Fun-stacking with JS, Python, Django, React, and Postgres. Math enthusiast. Author of https://t.co/xr8UoUKdi0",
+			"url": "https://t.co/URyxQ4lvHz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/URyxQ4lvHz",
+							"expanded_url": "https://www.neilwithdata.com/",
+							"display_url": "neilwithdata.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/xr8UoUKdi0",
+							"expanded_url": "http://MasteryWithSQL.com",
+							"display_url": "MasteryWithSQL.com",
+							"indices": [
+								121,
+								144
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 806,
+			"friends_count": 623,
+			"listed_count": 32,
+			"created_at": "Mon Mar 31 02:32:36 +0000 2008",
+			"favourites_count": 1078,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2215,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C130E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1133586386039316480/celLemgV_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1133586386039316480/celLemgV_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14262889/1581307242",
+			"profile_link_color": "838978",
+			"profile_sidebar_border_color": "9DB98C",
+			"profile_sidebar_fill_color": "140E0A",
+			"profile_text_color": "4F3F38",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 102,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@shl @gumroad https://t.co/BfxOHovXnR"
+	},
+	{
 		"created_at": "Wed Apr 01 13:09:23 +0000 2020",
 		"id": 1245329557399285800,
 		"id_str": "1245337464333463553",
