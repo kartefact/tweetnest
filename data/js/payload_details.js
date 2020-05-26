e1d2eeb20dfeafd54304a9f88b4c729c91a62eb6
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-05-26 05:42:30 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101115,
-	"created_at": "Wed May 20 00:12:29 +0000 2020",
+	"tweets": 101116,
+	"created_at": "Tue May 26 00:12:29 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Mon May 25 12:02:02 +0000 2020",
+		"id": 1264582703489314800,
+		"id_str": "1264889459553628162",
+		"full_text": "Doodsagar water falls,Karnataka -Goa border. Enjoy this 3D effect by closing one eye....😍👌 https://t.co/uwwPFzMUFO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1264582618214875100,
+					"id_str": "1264582618214875136",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1264582618214875136/pu/img/IYjrJCrNCspIKiIJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1264582618214875136/pu/img/IYjrJCrNCspIKiIJ.jpg",
+					"url": "https://t.co/uwwPFzMUFO",
+					"display_url": "pic.twitter.com/uwwPFzMUFO",
+					"expanded_url": "https://twitter.com/RajaniMPatil/status/1264582703489314821/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 352,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 352,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 352,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1264582618214875100,
+					"id_str": "1264582618214875136",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1264582618214875136/pu/img/IYjrJCrNCspIKiIJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1264582618214875136/pu/img/IYjrJCrNCspIKiIJ.jpg",
+					"url": "https://t.co/uwwPFzMUFO",
+					"display_url": "pic.twitter.com/uwwPFzMUFO",
+					"expanded_url": "https://twitter.com/RajaniMPatil/status/1264582703489314821/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 352,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 352,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 352,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							11,
+							20
+						],
+						"duration_millis": 58957,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1264582618214875136/pu/vid/320x580/UMpvMJbJCuUm-mKA.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1264582618214875136/pu/vid/352x640/9rOxpmuK3_Y3Y2CF.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1264582618214875136/pu/pl/yWIF9HPTKLZijOyk.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1202283265358356500,
+			"id_str": "1202283265358356480",
+			"name": "Rajani Patil",
+			"screen_name": "RajaniMPatil",
+			"location": "Bangalore",
+			"description": "Always smile😊 Expect little😐 Live simply 🤗",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 821,
+			"friends_count": 132,
+			"listed_count": 0,
+			"created_at": "Wed Dec 04 17:47:52 +0000 2019",
+			"favourites_count": 7619,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 751,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1263064037832617985/WOkD_u9y_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1263064037832617985/WOkD_u9y_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 383,
+		"favorite_count": 1429,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Doodsagar water falls,Karnataka -Goa border. Enjoy this 3D effect by closing one eye....😍👌 https://t.co/uwwPFzMUFO"
+	},
+	{
 		"created_at": "Tue May 19 22:42:18 +0000 2020",
 		"id": 1262717239377068000,
 		"id_str": "1262876259668746240",
