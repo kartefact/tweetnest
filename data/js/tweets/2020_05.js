d0a7e160cf107ba19e3777636d30e28a5911a566
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-06-01 05:42:30 +0530

    Added 1 tweets

--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,208 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 31 11:14:45 +0000 2020",
+		"id": 1267022402417881000,
+		"id_str": "1267051888245043201",
+		"full_text": "Similar situation everywhere. https://t.co/Nc2lXzcrTz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Nc2lXzcrTz",
+					"expanded_url": "https://twitter.com/RussInCheshire/status/1266938879300587520?s=19",
+					"display_url": "twitter.com/RussInCheshire…",
+					"indices": [
+						30,
+						53
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1685849366,
+			"id_str": "1685849366",
+			"name": "Mud n Sky",
+			"screen_name": "mudnsky",
+			"location": "India",
+			"description": "''This man has conquered the world! What have you done?'' ''I have conquered the need to conquer the world.'' - Pressfield",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 263,
+			"friends_count": 213,
+			"listed_count": 33,
+			"created_at": "Tue Aug 20 13:48:52 +0000 2013",
+			"favourites_count": 5612,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33930,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1262018540980207617/S3KRHxxz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1262018540980207617/S3KRHxxz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1685849366/1507642702",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1266938879300587500,
+		"quoted_status_id_str": "1266938879300587520",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Nc2lXzcrTz",
+			"expanded": "https://twitter.com/RussInCheshire/status/1266938879300587520?s=19",
+			"display": "twitter.com/RussInCheshire…"
+		},
+		"quoted_status": {
+			"created_at": "Sun May 31 03:45:42 +0000 2020",
+			"id": 1266938879300587500,
+			"id_str": "1266938879300587520",
+			"full_text": "When we had 335 new cases per day, it was considered bad enough to start the lockdown.\n\nWhen we have 2,445 new cases per day, it is considered good enough to end the lockdown.\n\nI can't be alone in questioning the logic of this. And I'm #NotGoingOut",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				248
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "NotGoingOut",
+						"indices": [
+							236,
+							248
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 498753368,
+				"id_str": "498753368",
+				"name": "Russ",
+				"screen_name": "RussInCheshire",
+				"location": "Cheshire",
+				"description": "Designer, coder, analyst, painter, wazzock. Feminist, centre-left, Remainer. Working class Manc in Cheshire.",
+				"url": "https://t.co/pRDoZ6wtCc",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/pRDoZ6wtCc",
+								"expanded_url": "https://russincheshire.wordpress.com/",
+								"display_url": "russincheshire.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 41551,
+				"friends_count": 1266,
+				"listed_count": 155,
+				"created_at": "Tue Feb 21 11:34:20 +0000 2012",
+				"favourites_count": 16353,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 66174,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "961C0C",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1205541805183447040/saidu0Ii_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1205541805183447040/saidu0Ii_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/498753368/1587419692",
+				"profile_link_color": "961C0C",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3387,
+			"favorite_count": 9764,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Similar situation everywhere. https://t.co/Nc2lXzcrTz"
+	},
+	{
 		"created_at": "Sat May 30 13:22:32 +0000 2020",
 		"id": 1266506282858680300,
 		"id_str": "1266721657319649280",
