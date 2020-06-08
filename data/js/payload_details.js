6d1751e5cb74c1177ec7f2dccb96923d4d55d41f
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-06-08 05:42:30 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101118,
-	"created_at": "Mon Jun 01 00:12:29 +0000 2020",
+	"tweets": 101119,
+	"created_at": "Mon Jun 08 00:12:29 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,177 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Sun Jun 07 05:03:42 +0000 2020",
+		"id": 1269235391351177200,
+		"id_str": "1269495224318873600",
+		"full_text": "When an ecologist marries an electrical engineer... https://t.co/aVf0PBbSPj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1269235335227195400,
+					"id_str": "1269235335227195392",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/EZ060AyXQAAoob6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZ060AyXQAAoob6.jpg",
+					"url": "https://t.co/aVf0PBbSPj",
+					"display_url": "pic.twitter.com/aVf0PBbSPj",
+					"expanded_url": "https://twitter.com/lalitpatnaik/status/1269235391351177216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 434,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 765,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1568,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1269235335227195400,
+					"id_str": "1269235335227195392",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/EZ060AyXQAAoob6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZ060AyXQAAoob6.jpg",
+					"url": "https://t.co/aVf0PBbSPj",
+					"display_url": "pic.twitter.com/aVf0PBbSPj",
+					"expanded_url": "https://twitter.com/lalitpatnaik/status/1269235391351177216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 434,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 765,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1568,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 140501827,
+			"id_str": "140501827",
+			"name": "Lalit Patnaik",
+			"screen_name": "lalitpatnaik",
+			"location": "Geneva, Switzerland",
+			"description": "Senior Fellow @CERN | Power Electronics Engineer | Dabbling in Physics and Philosophy | Observing Nature | Chronically Curious. Also tweeting @opelexnet",
+			"url": "https://t.co/blzw2gT44B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/blzw2gT44B",
+							"expanded_url": "http://chronicengineer.blogspot.com/",
+							"display_url": "chronicengineer.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 837,
+			"friends_count": 1326,
+			"listed_count": 24,
+			"created_at": "Wed May 05 17:13:44 +0000 2010",
+			"favourites_count": 22043,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3752,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/465378636820787200/kXuSk7P4_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/465378636820787200/kXuSk7P4_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/140501827/1535885073",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 87,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "When an ecologist marries an electrical engineer... https://t.co/aVf0PBbSPj"
+	}
+]
