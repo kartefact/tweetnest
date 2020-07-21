b67086ac6b114e0b1bd2941539cddb7c5fa50e5d
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-07-21 05:42:31 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101127,
-	"created_at": "Fri Jun 26 00:12:29 +0000 2020",
+	"tweets": 101128,
+	"created_at": "Tue Jul 21 00:12:29 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_07.js
@@ -0,0 +1,308 @@
+Grailbird.data.tweets_2020_07 = 
+[
+	{
+		"created_at": "Mon Jul 20 19:05:29 +0000 2020",
+		"id": 1285271322658914300,
+		"id_str": "1285289746646757376",
+		"full_text": "Sigh, I'll just leave this here https://t.co/VdI11mH2Ak https://t.co/09Si2Ug68W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VdI11mH2Ak",
+					"expanded_url": "https://www.pcmag.com/how-to/computer-acting-up-how-to-uninstall-a-windows-10-update",
+					"display_url": "pcmag.com/how-to/compute…",
+					"indices": [
+						32,
+						55
+					]
+				},
+				{
+					"url": "https://t.co/09Si2Ug68W",
+					"expanded_url": "https://twitter.com/pcgamer/status/1285271006068846593",
+					"display_url": "twitter.com/pcgamer/status…",
+					"indices": [
+						56,
+						79
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27870530,
+			"id_str": "27870530",
+			"name": "Whitson Gordon",
+			"screen_name": "WhitsonGordon",
+			"location": "San Diego, CA",
+			"description": "Freelance writer helping people use technology at @NBCNews, @PopSci, @PCMag, @IGN, @ItsTheInventory, and more. Past: editor-in-chief of @Lifehacker & @HowToGeek",
+			"url": "https://t.co/cFVFjORWOp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cFVFjORWOp",
+							"expanded_url": "http://whitsongordon.com",
+							"display_url": "whitsongordon.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16845,
+			"friends_count": 649,
+			"listed_count": 744,
+			"created_at": "Tue Mar 31 13:38:08 +0000 2009",
+			"favourites_count": 7116,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 16160,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/787001966937333760/g5Mnc2BR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/787001966937333760/g5Mnc2BR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27870530/1469026164",
+			"profile_link_color": "138B2E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "02B4B8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1285271006068846600,
+		"quoted_status_id_str": "1285271006068846593",
+		"quoted_status_permalink": {
+			"url": "https://t.co/09Si2Ug68W",
+			"expanded": "https://twitter.com/pcgamer/status/1285271006068846593",
+			"display": "twitter.com/pcgamer/status…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jul 20 17:51:01 +0000 2020",
+			"id": 1285271006068846600,
+			"id_str": "1285271006068846593",
+			"full_text": "Oh great, now Windows 10 is breaking the internet on some PCs  https://t.co/KwQ7A8ycK5 https://t.co/UZK1PgydAS",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				86
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/KwQ7A8ycK5",
+						"expanded_url": "https://buff.ly/39ciVAZ",
+						"display_url": "buff.ly/39ciVAZ",
+						"indices": [
+							63,
+							86
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1285271004521148400,
+						"id_str": "1285271004521148416",
+						"indices": [
+							87,
+							110
+						],
+						"media_url": "http://pbs.twimg.com/media/EdYzK0CXsAABRc1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EdYzK0CXsAABRc1.jpg",
+						"url": "https://t.co/UZK1PgydAS",
+						"display_url": "pic.twitter.com/UZK1PgydAS",
+						"expanded_url": "https://twitter.com/pcgamer/status/1285271006068846593/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1285271004521148400,
+						"id_str": "1285271004521148416",
+						"indices": [
+							87,
+							110
+						],
+						"media_url": "http://pbs.twimg.com/media/EdYzK0CXsAABRc1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EdYzK0CXsAABRc1.jpg",
+						"url": "https://t.co/UZK1PgydAS",
+						"display_url": "pic.twitter.com/UZK1PgydAS",
+						"expanded_url": "https://twitter.com/pcgamer/status/1285271006068846593/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16736535,
+				"id_str": "16736535",
+				"name": "PC Gamer",
+				"screen_name": "pcgamer",
+				"location": "San Francisco, Bath & Sydney",
+				"description": "The global authority on PC games. Tweets from our US, UK, and AU editors.",
+				"url": "https://t.co/C4FGsPK7FD",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/C4FGsPK7FD",
+								"expanded_url": "http://www.pcgamer.com",
+								"display_url": "pcgamer.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2068780,
+				"friends_count": 85,
+				"listed_count": 5780,
+				"created_at": "Tue Oct 14 11:19:09 +0000 2008",
+				"favourites_count": 965,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 86406,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/877980023025803270/xntVDuTq_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/877980023025803270/xntVDuTq_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16736535/1561494498",
+				"profile_link_color": "DC1E25",
+				"profile_sidebar_border_color": "939598",
+				"profile_sidebar_fill_color": "E6E6E6",
+				"profile_text_color": "000F18",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 63,
+			"favorite_count": 437,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sigh, I'll just leave this here https://t.co/VdI11mH2Ak https://t.co/09Si2Ug68W"
+	}
+]
