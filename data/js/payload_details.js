3210ea677a90a8cbd412bf0f946bf4e0970ef71f
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-08-21 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101130,
-	"created_at": "Wed Aug 12 00:12:28 +0000 2020",
+	"tweets": 101131,
+	"created_at": "Fri Aug 21 00:12:28 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,296 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Thu Aug 20 00:53:27 +0000 2020",
+		"id": 1296112166010065000,
+		"id_str": "1296248948588285952",
+		"full_text": "How to build a 2 trillion dollar company: #Apple https://t.co/8RBfFoZYjz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Apple",
+					"indices": [
+						42,
+						48
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1296112080622432300,
+					"id_str": "1296112080622432258",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/Efy3EfqXsAIpDps.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Efy3EfqXsAIpDps.jpg",
+					"url": "https://t.co/8RBfFoZYjz",
+					"display_url": "pic.twitter.com/8RBfFoZYjz",
+					"expanded_url": "https://twitter.com/nixcraft/status/1296112166010064898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 406,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 430,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 430,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1296112080622432300,
+					"id_str": "1296112080622432258",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/Efy3EfqXsAIpDps.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Efy3EfqXsAIpDps.jpg",
+					"url": "https://t.co/8RBfFoZYjz",
+					"display_url": "pic.twitter.com/8RBfFoZYjz",
+					"expanded_url": "https://twitter.com/nixcraft/status/1296112166010064898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 406,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 430,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 430,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1296112080597246000,
+					"id_str": "1296112080597245954",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/Efy3EfkXYAIIrGC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Efy3EfkXYAIIrGC.jpg",
+					"url": "https://t.co/8RBfFoZYjz",
+					"display_url": "pic.twitter.com/8RBfFoZYjz",
+					"expanded_url": "https://twitter.com/nixcraft/status/1296112166010064898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1296112080618217500,
+					"id_str": "1296112080618217472",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/Efy3EfpXYAA9y0X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Efy3EfpXYAA9y0X.jpg",
+					"url": "https://t.co/8RBfFoZYjz",
+					"display_url": "pic.twitter.com/8RBfFoZYjz",
+					"expanded_url": "https://twitter.com/nixcraft/status/1296112166010064898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1296112080626516000,
+					"id_str": "1296112080626515969",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/Efy3EfrWAAEVN0e.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Efy3EfrWAAEVN0e.jpg",
+					"url": "https://t.co/8RBfFoZYjz",
+					"display_url": "pic.twitter.com/8RBfFoZYjz",
+					"expanded_url": "https://twitter.com/nixcraft/status/1296112166010064898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 736,
+							"h": 736,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 736,
+							"h": 736,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17484680,
+			"id_str": "17484680",
+			"name": "The Best Linux Blog In the Unixverse",
+			"screen_name": "nixcraft",
+			"location": "Rootland",
+			"description": "Enjoy Linux & Unix-like systems? Open source software and programming? Sysadmin & DevOps work? Follow us to make the most of sysadmin-hood.",
+			"url": "https://t.co/dwH1Fatmvu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dwH1Fatmvu",
+							"expanded_url": "https://www.cyberciti.biz/",
+							"display_url": "cyberciti.biz",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 189044,
+			"friends_count": 388,
+			"listed_count": 3486,
+			"created_at": "Wed Nov 19 12:12:37 +0000 2008",
+			"favourites_count": 37243,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 44344,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ACDED6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1209802752819261440/jvir-xNm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1209802752819261440/jvir-xNm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17484680/1486494500",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2920,
+		"favorite_count": 8540,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "How to build a 2 trillion dollar company: #Apple https://t.co/8RBfFoZYjz"
+	},
+	{
 		"created_at": "Tue Aug 11 04:22:25 +0000 2020",
 		"id": 1293040047403327500,
 		"id_str": "1293040047403327488",
