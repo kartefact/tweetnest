54a2d20c1694839f5eea485f754ce75b248af1da
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-09-10 05:42:29 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101132,
-	"created_at": "Wed Sep 09 00:12:28 +0000 2020",
+	"tweets": 101133,
+	"created_at": "Thu Sep 10 00:12:28 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,182 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Wed Sep 09 05:41:46 +0000 2020",
+		"id": 1303523894582677500,
+		"id_str": "1303569266214543360",
+		"full_text": "Going to show this to my kid today. Happy Wednesday folks. #WednesdayWisdom #WednesdayThoughts https://t.co/QUbJjQmWqC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WednesdayWisdom",
+					"indices": [
+						59,
+						75
+					]
+				},
+				{
+					"text": "WednesdayThoughts",
+					"indices": [
+						76,
+						94
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1303523884256301000,
+					"id_str": "1303523884256301056",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EhcMEP1VkAAiS_x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EhcMEP1VkAAiS_x.jpg",
+					"url": "https://t.co/QUbJjQmWqC",
+					"display_url": "pic.twitter.com/QUbJjQmWqC",
+					"expanded_url": "https://twitter.com/carona_m/status/1303523894582677505/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1303523884256301000,
+					"id_str": "1303523884256301056",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EhcMEP1VkAAiS_x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EhcMEP1VkAAiS_x.jpg",
+					"url": "https://t.co/QUbJjQmWqC",
+					"display_url": "pic.twitter.com/QUbJjQmWqC",
+					"expanded_url": "https://twitter.com/carona_m/status/1303523894582677505/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15902653,
+			"id_str": "15902653",
+			"name": "Carona Mohapatra",
+			"screen_name": "carona_m",
+			"location": "Bangalore, India",
+			"description": "Marketer, Budding Entrepreneur, Crazy Mom, Book & Coffee Addict. Small town girl from Odisha, now a Bangalorean.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1162,
+			"friends_count": 875,
+			"listed_count": 51,
+			"created_at": "Tue Aug 19 10:16:40 +0000 2008",
+			"favourites_count": 4595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 12972,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1149478849089949697/WAmqSQ1v_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1149478849089949697/WAmqSQ1v_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15902653/1570892662",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Going to show this to my kid today. Happy Wednesday folks. #WednesdayWisdom #WednesdayThoughts https://t.co/QUbJjQmWqC"
+	},
+	{
 		"created_at": "Tue Sep 08 21:20:02 +0000 2020",
 		"id": 1303442998856880000,
 		"id_str": "1303442998856880128",
