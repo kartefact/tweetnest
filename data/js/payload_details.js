ef71511040abc6568017978dbc344bba6029d4b1
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-10-02 05:31:33 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101137,
-	"created_at": "Fri Sep 25 00:01:32 +0000 2020",
+	"tweets": 101138,
+	"created_at": "Fri Oct 02 00:01:32 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_10.js
@@ -0,0 +1,201 @@
+Grailbird.data.tweets_2020_10 = 
+[
+	{
+		"created_at": "Thu Oct 01 20:24:14 +0000 2020",
+		"id": 1311497468559974400,
+		"id_str": "1311763879593078784",
+		"full_text": "@coderloco We feel you. https://t.co/9i5lBASX7G",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "coderloco",
+					"name": "Coder Loco",
+					"id": 1251990474,
+					"id_str": "1251990474",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1311497444916686800,
+					"id_str": "1311497444916686853",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EjNf-kAXcAUFqt6.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EjNf-kAXcAUFqt6.jpg",
+					"url": "https://t.co/9i5lBASX7G",
+					"display_url": "pic.twitter.com/9i5lBASX7G",
+					"expanded_url": "https://twitter.com/msexcel/status/1311497468559974400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 288,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 288,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 288,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1311497444916686800,
+					"id_str": "1311497444916686853",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EjNf-kAXcAUFqt6.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EjNf-kAXcAUFqt6.jpg",
+					"url": "https://t.co/9i5lBASX7G",
+					"display_url": "pic.twitter.com/9i5lBASX7G",
+					"expanded_url": "https://twitter.com/msexcel/status/1311497468559974400/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 288,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 288,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 288,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							3,
+							5
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EjNf-kAXcAUFqt6.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://prod1.sprinklr.com\" rel=\"nofollow\">Sprinklr Publishing</a>",
+		"in_reply_to_status_id": 1311459392752103400,
+		"in_reply_to_status_id_str": "1311459392752103426",
+		"in_reply_to_user_id": 1251990474,
+		"in_reply_to_user_id_str": "1251990474",
+		"in_reply_to_screen_name": "coderloco",
+		"user": {
+			"id": 15934026,
+			"id_str": "15934026",
+			"name": "Microsoft Excel",
+			"screen_name": "msexcel",
+			"location": "Redmond, WA",
+			"description": "Master your data to achieve more with Microsoft Excel.",
+			"url": "http://t.co/HcFOqOg4FX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/HcFOqOg4FX",
+							"expanded_url": "http://office.microsoft.com/excel",
+							"display_url": "office.microsoft.com/excel",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 152616,
+			"friends_count": 179,
+			"listed_count": 1451,
+			"created_at": "Thu Aug 21 17:27:45 +0000 2008",
+			"favourites_count": 4440,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 70173,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "4A913C",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1129474049199882240/RZPZ1KYT_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1129474049199882240/RZPZ1KYT_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15934026/1598631685",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 660,
+		"favorite_count": 2248,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@coderloco We feel you. https://t.co/9i5lBASX7G"
+	}
+]
