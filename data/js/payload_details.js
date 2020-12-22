5102a13ca7c2bef2cd8153639fd3baebd4bcdc2c
Karthikeyan Ravikumar <kartefact@gmail.com>
2020-12-22 05:31:33 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101151,
-	"created_at": "Fri Dec 04 00:01:32 +0000 2020",
+	"tweets": 101152,
+	"created_at": "Tue Dec 22 00:01:32 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Mon Dec 21 18:13:26 +0000 2020",
+		"id": 1340745731406119000,
+		"id_str": "1341084379527856128",
+		"full_text": "Little do people know that 𝐈𝐧𝐝𝐢𝐚 has silently built the best 𝐝𝐢𝐠𝐢𝐭𝐚𝐥 𝐩𝐚𝐲𝐦𝐞𝐧𝐭𝐬 infrastructure in the world.\n\nWhile most of the world is still using credit cards and cash, India has 𝐥𝐞𝐚𝐩-𝐟𝐫𝐨𝐠𝐠𝐞𝐝 into the future.\n\nHere is how it all started... https://t.co/g8Nw71MtMO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			240
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1340727247481630700,
+					"id_str": "1340727247481630720",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/Eps4U_7UcAACumc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eps4U_7UcAACumc.jpg",
+					"url": "https://t.co/g8Nw71MtMO",
+					"display_url": "pic.twitter.com/g8Nw71MtMO",
+					"expanded_url": "https://twitter.com/iam_preethi/status/1340745731406118913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1340727247481630700,
+					"id_str": "1340727247481630720",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/Eps4U_7UcAACumc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eps4U_7UcAACumc.jpg",
+					"url": "https://t.co/g8Nw71MtMO",
+					"display_url": "pic.twitter.com/g8Nw71MtMO",
+					"expanded_url": "https://twitter.com/iam_preethi/status/1340745731406118913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1478433775,
+			"id_str": "1478433775",
+			"name": "Preethi Kasireddy",
+			"screen_name": "iam_preethi",
+			"location": "TBU",
+			"description": "https://t.co/jZd0L6zJKt\n\nPreviously @a16z, @coinbase, @GoldmanSachs, @isTruStory\n\nSign up for my new course on cryptocurrency 👇",
+			"url": "https://t.co/b2jfFHy2l1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b2jfFHy2l1",
+							"expanded_url": "https://www.preethikasireddy.com/intro-to-cryptocurrency",
+							"display_url": "preethikasireddy.com/intro-to-crypt…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/jZd0L6zJKt",
+							"expanded_url": "http://preethikasireddy.com",
+							"display_url": "preethikasireddy.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 84791,
+			"friends_count": 434,
+			"listed_count": 1897,
+			"created_at": "Sun Jun 02 23:14:27 +0000 2013",
+			"favourites_count": 5087,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5835,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1329861922829840388/u7T2d35__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1329861922829840388/u7T2d35__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1478433775/1442198556",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 795,
+		"favorite_count": 2605,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Little do people know that 𝐈𝐧𝐝𝐢𝐚 has silently built the best 𝐝𝐢𝐠𝐢𝐭𝐚𝐥 𝐩𝐚𝐲𝐦𝐞𝐧𝐭𝐬 infrastructure in the world.\n\nWhile most of the world is still using credit cards and cash, India has 𝐥𝐞𝐚𝐩-𝐟𝐫𝐨𝐠𝐠𝐞𝐝 into the future.\n\nHere is how it all started... https://t.co/g8Nw71MtMO"
+	},
+	{
 		"created_at": "Thu Dec 03 06:59:10 +0000 2020",
 		"id": 1334391711154360300,
 		"id_str": "1334391711154360321",
