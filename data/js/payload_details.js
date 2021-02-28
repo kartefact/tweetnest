14dfc5dea674f268ac258f10b1750691efd2f6de
Karthikeyan Ravikumar <kartefact@gmail.com>
2021-02-28 05:31:32 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101154,
-	"created_at": "Sat Feb 13 00:01:31 +0000 2021",
+	"tweets": 101155,
+	"created_at": "Sun Feb 28 00:01:31 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,267 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Sat Feb 27 04:28:41 +0000 2021",
+		"id": 1365507084297633800,
+		"id_str": "1365519199297146885",
+		"full_text": "Mathematical Dance.\nhttps://t.co/EN7mszCmG4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			43
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1170739782772830200,
+					"id_str": "1170739782772830208",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1170739782772830208/pu/img/0NTdJEVoso6CRrXA.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1170739782772830208/pu/img/0NTdJEVoso6CRrXA.jpg",
+					"url": "https://t.co/EN7mszCmG4",
+					"display_url": "pic.twitter.com/EN7mszCmG4",
+					"expanded_url": "https://twitter.com/eyeofastronomy/status/1170739819431038977/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 516,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 516,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 516,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1170739819431039000,
+					"source_status_id_str": "1170739819431038977",
+					"source_user_id": 892016550827446300,
+					"source_user_id_str": "892016550827446272"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1170739782772830200,
+					"id_str": "1170739782772830208",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1170739782772830208/pu/img/0NTdJEVoso6CRrXA.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1170739782772830208/pu/img/0NTdJEVoso6CRrXA.jpg",
+					"url": "https://t.co/EN7mszCmG4",
+					"display_url": "pic.twitter.com/EN7mszCmG4",
+					"expanded_url": "https://twitter.com/eyeofastronomy/status/1170739819431038977/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 516,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 516,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 516,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1170739819431039000,
+					"source_status_id_str": "1170739819431038977",
+					"source_user_id": 892016550827446300,
+					"source_user_id_str": "892016550827446272",
+					"video_info": {
+						"aspect_ratio": [
+							40,
+							43
+						],
+						"duration_millis": 8034,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1170739782772830208/pu/vid/360x386/Wvjhnb5uHLP0JWsV.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1170739782772830208/pu/vid/480x516/2X5pTz2G8rR4Z-qd.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1170739782772830208/pu/vid/320x344/HP4mCFgipZTE6m1m.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1170739782772830208/pu/pl/_ZhXefp9ftnN8laR.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 892016550827446300,
+							"id_str": "892016550827446272",
+							"name": "The Mars Girl",
+							"screen_name": "eyeofastronomy",
+							"location": "Earth",
+							"description": "Aspiring Astronaut | My goal is simple. It is a complete understanding of the universe, why it is as it is and why it exists at all✨\n☆Let's Explore🚀\n#NASA",
+							"url": "https://t.co/jEi3sz5nTD",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "https://t.co/jEi3sz5nTD",
+											"expanded_url": "https://www.instagram.com/astro_iram/",
+											"display_url": "instagram.com/astro_iram/",
+											"indices": [
+												0,
+												23
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 12691,
+							"friends_count": 153,
+							"listed_count": 131,
+							"created_at": "Mon Jul 31 13:38:13 +0000 2017",
+							"favourites_count": 3105,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": false,
+							"verified": false,
+							"statuses_count": 1285,
+							"lang": null,
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "000000",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/1131961691061792769/OKjwjCMY_normal.png",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/1131961691061792769/OKjwjCMY_normal.png",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/892016550827446272/1605974127",
+							"profile_link_color": "981CEB",
+							"profile_sidebar_border_color": "000000",
+							"profile_sidebar_fill_color": "000000",
+							"profile_text_color": "000000",
+							"profile_use_background_image": false,
+							"has_extended_profile": true,
+							"default_profile": false,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1272330881894670300,
+			"id_str": "1272330881894670341",
+			"name": "Tech Burrito",
+			"screen_name": "TechAmazing",
+			"location": "",
+			"description": "Wisdom | Tech | Learning \n\n\"Any sufficiently advanced technology is equivalent to magic.\" \n– Sir Arthur C. Clarke\n\nPartnership info@proandromeda.com",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 140045,
+			"friends_count": 38,
+			"listed_count": 753,
+			"created_at": "Mon Jun 15 00:52:19 +0000 2020",
+			"favourites_count": 1045,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1902,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1272513897447264267/_7KnHbcM_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1272513897447264267/_7KnHbcM_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1272330881894670341/1592182561",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2922,
+		"favorite_count": 11801,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Mathematical Dance.\nhttps://t.co/EN7mszCmG4"
+	},
+	{
 		"created_at": "Fri Feb 12 21:10:55 +0000 2021",
 		"id": 1360190478940049400,
 		"id_str": "1360335599442382850",
