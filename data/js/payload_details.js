64cc0318497741ff9cd39440c5176dd2c9c6f136
Karthikeyan Ravikumar <kartefact@gmail.com>
2021-04-10 05:31:32 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101176,
-	"created_at": "Wed Mar 31 00:01:31 +0000 2021",
+	"tweets": 101177,
+	"created_at": "Sat Apr 10 00:01:31 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Fri Apr 09 17:22:19 +0000 2021",
+		"id": 1380511871619408000,
+		"id_str": "1380571793329557505",
+		"full_text": "One of the biggest reasons developers (and not just PMs and designers) should speak to the users/ customers of their products is simple - the feedback from a user will make you take great pride in your work. A delighted user is all the motivation you need to keep devs engaged.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 110762005,
+			"id_str": "110762005",
+			"name": "Anshumani Ruddra",
+			"screen_name": "baboonzero",
+			"location": "Bangalore, India",
+			"description": "Tinkerer. Fixer. Product Builder. App PM Lead for Google Pay.",
+			"url": "https://t.co/DOhvP5wilM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DOhvP5wilM",
+							"expanded_url": "http://anshumani.com",
+							"display_url": "anshumani.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8009,
+			"friends_count": 230,
+			"listed_count": 101,
+			"created_at": "Tue Feb 02 17:50:33 +0000 2010",
+			"favourites_count": 2782,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2457,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1211544324904636417/-aquX_Ly_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1211544324904636417/-aquX_Ly_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/110762005/1454730992",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 98,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "One of the biggest reasons developers (and not just PMs and designers) should speak to the users/ customers of their products is simple - the feedback from a user will make you take great pride in your work. A delighted user is all the motivation you need to keep devs engaged."
+	}
+]
