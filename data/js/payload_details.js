a7fe18d549359fe34d9759198bebb37f627cdaef
Karthikeyan Ravikumar <kartefact@gmail.com>
2021-04-19 05:31:31 +0530

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101177,
-	"created_at": "Sat Apr 10 00:01:31 +0000 2021",
+	"tweets": 101179,
+	"created_at": "Mon Apr 19 00:01:31 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,277 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Sun Apr 18 08:36:38 +0000 2021",
+		"id": 1383700988452753400,
+		"id_str": "1383700988452753410",
+		"full_text": "Here’s a song for you… Walking On Sunshine by Katrina &amp; The Waves\nhttps://t.co/2lxn39yvaE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2lxn39yvaE",
+					"expanded_url": "https://open.spotify.com/track/05wIrZSwuaVWhcv5FfqeH0?si=FZgjxINLQq2r_qlud1_sDw",
+					"display_url": "open.spotify.com/track/05wIrZSw…",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1439,
+			"friends_count": 214,
+			"listed_count": 47,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10401,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 99221,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Here’s a song for you… Walking On Sunshine by Katrina &amp; The Waves\nhttps://t.co/2lxn39yvaE"
+	},
+	{
+		"created_at": "Sun Apr 18 08:30:02 +0000 2021",
+		"id": 1383637205902499800,
+		"id_str": "1383699330389839872",
+		"full_text": "Friends mother in #delhi is in urgent need of AB+ plasma. BL Kapoor hospital, Pusa Road. Please retweet and amplify. #COVID19India https://t.co/yj6HjmAxlM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			130
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "delhi",
+					"indices": [
+						18,
+						24
+					]
+				},
+				{
+					"text": "COVID19India",
+					"indices": [
+						117,
+						130
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1383637199187439600,
+					"id_str": "1383637199187439627",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/media/EzOqs6uVcAsvKAn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzOqs6uVcAsvKAn.jpg",
+					"url": "https://t.co/yj6HjmAxlM",
+					"display_url": "pic.twitter.com/yj6HjmAxlM",
+					"expanded_url": "https://twitter.com/arfunnnnn/status/1383637205902499843/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 730,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1246,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 414,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1383637199187439600,
+					"id_str": "1383637199187439627",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/media/EzOqs6uVcAsvKAn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzOqs6uVcAsvKAn.jpg",
+					"url": "https://t.co/yj6HjmAxlM",
+					"display_url": "pic.twitter.com/yj6HjmAxlM",
+					"expanded_url": "https://twitter.com/arfunnnnn/status/1383637205902499843/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 730,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1246,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 414,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24950679,
+			"id_str": "24950679",
+			"name": "Arpan",
+			"screen_name": "arfunnnnn",
+			"location": "Bangalore,India",
+			"description": "Grammar Nazi in the works. Preferred pronouns(your/you're). I tweet music, politics, Chelsea FC and my undying love for the Indian RW.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2436,
+			"friends_count": 682,
+			"listed_count": 38,
+			"created_at": "Tue Mar 17 20:44:22 +0000 2009",
+			"favourites_count": 11661,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90339,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1349751159255228417/E9KFHhRc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1349751159255228417/E9KFHhRc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24950679/1610640693",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 99,
+		"favorite_count": 82,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Friends mother in #delhi is in urgent need of AB+ plasma. BL Kapoor hospital, Pusa Road. Please retweet and amplify. #COVID19India https://t.co/yj6HjmAxlM"
+	},
+	{
 		"created_at": "Fri Apr 09 17:22:19 +0000 2021",
 		"id": 1380511871619408000,
 		"id_str": "1380571793329557505",
