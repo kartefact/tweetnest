21c65c9692946632ec38fff57d4dca7b808661d6
Karthikeyan Ravikumar <kartefact@gmail.com>
2021-05-25 05:31:32 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101181,
-	"created_at": "Sun May 09 00:01:31 +0000 2021",
+	"tweets": 101182,
+	"created_at": "Tue May 25 00:01:31 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Mon May 24 20:34:53 +0000 2021",
+		"id": 1395072035114553300,
+		"id_str": "1396927707884126208",
+		"full_text": "After the rains inside IISC Campus, Bengaluru https://t.co/nQ6vl44o5H",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			45
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1395071484733784000,
+					"id_str": "1395071484733784069",
+					"indices": [
+						46,
+						69
+					],
+					"media_url": "http://pbs.twimg.com/media/E1xKH7vVgAUCrqh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E1xKH7vVgAUCrqh.jpg",
+					"url": "https://t.co/nQ6vl44o5H",
+					"display_url": "pic.twitter.com/nQ6vl44o5H",
+					"expanded_url": "https://twitter.com/moon_chandani/status/1395072035114553346/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1077,
+							"h": 1346,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1395071484733784000,
+					"id_str": "1395071484733784069",
+					"indices": [
+						46,
+						69
+					],
+					"media_url": "http://pbs.twimg.com/media/E1xKH7vVgAUCrqh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E1xKH7vVgAUCrqh.jpg",
+					"url": "https://t.co/nQ6vl44o5H",
+					"display_url": "pic.twitter.com/nQ6vl44o5H",
+					"expanded_url": "https://twitter.com/moon_chandani/status/1395072035114553346/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1077,
+							"h": 1346,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4380344297,
+			"id_str": "4380344297",
+			"name": "Chandni",
+			"screen_name": "moon_chandani",
+			"location": "no. location",
+			"description": "Meeting people who inspire and encourage your creative mind #AnimalsLover #Nature #Cooking #Music \nStrictly No DM  Tag Me= Means Block \nAdvocate",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 31228,
+			"friends_count": 202,
+			"listed_count": 355,
+			"created_at": "Sat Dec 05 06:46:02 +0000 2015",
+			"favourites_count": 173807,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 277207,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1392058928704327682/HucBy79W_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1392058928704327682/HucBy79W_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4380344297/1605634489",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3083,
+		"favorite_count": 44131,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "After the rains inside IISC Campus, Bengaluru https://t.co/nQ6vl44o5H"
+	},
+	{
 		"created_at": "Sat May 08 05:08:04 +0000 2021",
 		"id": 1390896257636335600,
 		"id_str": "1390896257636335617",
