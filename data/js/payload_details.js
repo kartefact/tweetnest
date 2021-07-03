0c60be41f033abf9a2204ee1786b32e6275bc1a1
Karthikeyan Ravikumar <kartefact@gmail.com>
2021-07-03 05:31:31 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101184,
-	"created_at": "Fri Jun 25 00:01:31 +0000 2021",
+	"tweets": 101185,
+	"created_at": "Sat Jul 03 00:01:31 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_07.js
@@ -0,0 +1,107 @@
+Grailbird.data.tweets_2021_07 = 
+[
+	{
+		"created_at": "Fri Jul 02 05:23:07 +0000 2021",
+		"id": 1410831380099985400,
+		"id_str": "1410831380099985410",
+		"full_text": "@iBakasura @mudnsky Another personal qualm of mine with many snack brands is not having different sized packs. Having that helps a lot. Gives the options to go for tiny ones (something at ₹5/₹10) preventing storage hassle, jumbo ones for parties and mid sized ones for regular days.",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			282
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "iBakasura",
+					"name": "Tj",
+					"id": 115513801,
+					"id_str": "115513801",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "mudnsky",
+					"name": "Mud n Sky",
+					"id": 1685849366,
+					"id_str": "1685849366",
+					"indices": [
+						11,
+						19
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1410601917911158800,
+		"in_reply_to_status_id_str": "1410601917911158785",
+		"in_reply_to_user_id": 115513801,
+		"in_reply_to_user_id_str": "115513801",
+		"in_reply_to_screen_name": "iBakasura",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1416,
+			"friends_count": 214,
+			"listed_count": 47,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10327,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 99164,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@iBakasura @mudnsky Another personal qualm of mine with many snack brands is not having different sized packs. Having that helps a lot. Gives the options to go for tiny ones (something at ₹5/₹10) preventing storage hassle, jumbo ones for parties and mid sized ones for regular days."
+	}
+]
