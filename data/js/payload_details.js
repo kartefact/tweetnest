05cd1c1455fe2803a930e3abfab7da4da6ca913e
Karthikeyan Ravikumar <kartefact@gmail.com>
2021-10-03 05:31:31 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101199,
-	"created_at": "Sun Sep 05 00:01:31 +0000 2021",
+	"tweets": 101200,
+	"created_at": "Sun Oct 03 00:01:30 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_10.js
@@ -0,0 +1,286 @@
+Grailbird.data.tweets_2021_10 = 
+[
+	{
+		"created_at": "Sat Oct 02 10:32:19 +0000 2021",
+		"id": 1443584902352408600,
+		"id_str": "1444248874160295939",
+		"full_text": "This is basically kadubu or tumbler idli (cylindrical idli steamed in steel tumblers or wrapped in jackfruit leaves). A specialty of Udupi and Mangalore, coastal Deccan cuisine. The popsicle stick is a nice fast food innovation! #approve https://t.co/CuwXrB5upf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			237
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "approve",
+					"indices": [
+						229,
+						237
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CuwXrB5upf",
+					"expanded_url": "https://twitter.com/pal36/status/1443464112961298435",
+					"display_url": "twitter.com/pal36/status/1…",
+					"indices": [
+						238,
+						261
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 836350213,
+			"id_str": "836350213",
+			"name": "Rajini Rao",
+			"screen_name": "madamscientist",
+			"location": "Baltimore, USA",
+			"description": "Professor and Director of @CMM_JHU at Johns Hopkins. Ion transporter aficionado, experimental cook, advocate for #STEMWomen. Personal account.",
+			"url": "https://t.co/SWDzAhbYvY",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SWDzAhbYvY",
+							"expanded_url": "http://www.bs.jhmi.edu/physiology/raolab/home.html",
+							"display_url": "bs.jhmi.edu/physiology/rao…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6950,
+			"friends_count": 1541,
+			"listed_count": 153,
+			"created_at": "Thu Sep 20 20:57:23 +0000 2012",
+			"favourites_count": 140855,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23776,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2631473145/78fd8b4d4603b0e2b8d03ad68009ee20_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2631473145/78fd8b4d4603b0e2b8d03ad68009ee20_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/836350213/1357750078",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1443464112961298400,
+		"quoted_status_id_str": "1443464112961298435",
+		"quoted_status_permalink": {
+			"url": "https://t.co/CuwXrB5upf",
+			"expanded": "https://twitter.com/pal36/status/1443464112961298435",
+			"display": "twitter.com/pal36/status/1…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Sep 30 06:33:57 +0000 2021",
+			"id": 1443464112961298400,
+			"id_str": "1443464112961298435",
+			"full_text": "Just one question, why?? https://t.co/lH6lAA7r39",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				24
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1443464106674045000,
+						"id_str": "1443464106674044930",
+						"indices": [
+							25,
+							48
+						],
+						"media_url": "http://pbs.twimg.com/media/FAg29QxVQAIcCm-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FAg29QxVQAIcCm-.jpg",
+						"url": "https://t.co/lH6lAA7r39",
+						"display_url": "pic.twitter.com/lH6lAA7r39",
+						"expanded_url": "https://twitter.com/pal36/status/1443464112961298435/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 617,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 720,
+								"h": 793,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 793,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1443464106674045000,
+						"id_str": "1443464106674044930",
+						"indices": [
+							25,
+							48
+						],
+						"media_url": "http://pbs.twimg.com/media/FAg29QxVQAIcCm-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FAg29QxVQAIcCm-.jpg",
+						"url": "https://t.co/lH6lAA7r39",
+						"display_url": "pic.twitter.com/lH6lAA7r39",
+						"expanded_url": "https://twitter.com/pal36/status/1443464112961298435/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 617,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 720,
+								"h": 793,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 793,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 45805012,
+				"id_str": "45805012",
+				"name": "Micro-ambitious",
+				"screen_name": "pal36",
+				"location": "",
+				"description": "Very specific about my toothbrush color.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 25450,
+				"friends_count": 3434,
+				"listed_count": 302,
+				"created_at": "Tue Jun 09 09:53:47 +0000 2009",
+				"favourites_count": 3476,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 137240,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1413153794938982400/0lXgSS4O_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1413153794938982400/0lXgSS4O_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/45805012/1625757480",
+				"profile_link_color": "A34457",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 276,
+			"favorite_count": 2756,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 46,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is basically kadubu or tumbler idli (cylindrical idli steamed in steel tumblers or wrapped in jackfruit leaves). A specialty of Udupi and Mangalore, coastal Deccan cuisine. The popsicle stick is a nice fast food innovation! #approve https://t.co/CuwXrB5upf"
+	}
+]
