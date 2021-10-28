21c8e7528ccd69d6ed3332c766fdc1d5e7d92031
Karthikeyan Ravikumar <kartefact@gmail.com>
2021-10-28 05:31:32 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101206,
-	"created_at": "Sun Oct 24 00:01:31 +0000 2021",
+	"tweets": 101207,
+	"created_at": "Thu Oct 28 00:01:31 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Wed Oct 27 22:12:55 +0000 2021",
+		"id": 1453484884274516000,
+		"id_str": "1453484884274515974",
+		"full_text": "Do we even need powerful laptops anymore?\n\nSpeed on Windows on Azure: https://t.co/FHplYrX8s3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			69
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1453484851051450400,
+					"id_str": "1453484851051450374",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/media/FCvQxWlVUAYdqfS.png",
+					"media_url_https": "https://pbs.twimg.com/media/FCvQxWlVUAYdqfS.png",
+					"url": "https://t.co/FHplYrX8s3",
+					"display_url": "pic.twitter.com/FHplYrX8s3",
+					"expanded_url": "https://twitter.com/kartefact/status/1453484884274515974/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 93,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 723,
+							"h": 99,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 99,
+							"h": 99,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 723,
+							"h": 99,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1453484851051450400,
+					"id_str": "1453484851051450374",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/media/FCvQxWlVUAYdqfS.png",
+					"media_url_https": "https://pbs.twimg.com/media/FCvQxWlVUAYdqfS.png",
+					"url": "https://t.co/FHplYrX8s3",
+					"display_url": "pic.twitter.com/FHplYrX8s3",
+					"expanded_url": "https://twitter.com/kartefact/status/1453484884274515974/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 93,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 723,
+							"h": 99,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 99,
+							"h": 99,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 723,
+							"h": 99,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1411,
+			"friends_count": 217,
+			"listed_count": 47,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10259,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 99085,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Do we even need powerful laptops anymore?\n\nSpeed on Windows on Azure: https://t.co/FHplYrX8s3"
+	},
+	{
 		"created_at": "Sat Oct 23 03:54:15 +0000 2021",
 		"id": 1451758842874794000,
 		"id_str": "1451758842874793988",
