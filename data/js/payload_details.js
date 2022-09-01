8fa71ade7e774e3b664661d997818bcf7fbe1a6f
Karthikeyan Ravikumar <kartefact@gmail.com>
2022-09-01 05:31:32 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101221,
-	"created_at": "Wed Apr 27 00:01:31 +0000 2022",
+	"tweets": 101222,
+	"created_at": "Thu Sep 01 00:01:31 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_08.js
@@ -0,0 +1,196 @@
+Grailbird.data.tweets_2022_08 = 
+[
+	{
+		"created_at": "Wed Aug 31 15:22:19 +0000 2022",
+		"id": 1564704800494715000,
+		"id_str": "1564997017381658624",
+		"full_text": "This guy wins at Linkedin https://t.co/EPaJLROGtY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			25
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1564704684954230800,
+					"id_str": "1564704684954230786",
+					"indices": [
+						26,
+						49
+					],
+					"media_url": "http://pbs.twimg.com/media/Fbbynu0WIAIh3w7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fbbynu0WIAIh3w7.jpg",
+					"url": "https://t.co/EPaJLROGtY",
+					"display_url": "pic.twitter.com/EPaJLROGtY",
+					"expanded_url": "https://twitter.com/ChrisJBakke/status/1564704800494714883/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1056,
+							"h": 1298,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 553,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 976,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1564704684954230800,
+					"id_str": "1564704684954230786",
+					"indices": [
+						26,
+						49
+					],
+					"media_url": "http://pbs.twimg.com/media/Fbbynu0WIAIh3w7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fbbynu0WIAIh3w7.jpg",
+					"url": "https://t.co/EPaJLROGtY",
+					"display_url": "pic.twitter.com/EPaJLROGtY",
+					"expanded_url": "https://twitter.com/ChrisJBakke/status/1564704800494714883/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1056,
+							"h": 1298,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 553,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 976,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1361124510,
+			"id_str": "1361124510",
+			"name": "Chris Bakke",
+			"screen_name": "ChrisJBakke",
+			"location": "Danville, CA",
+			"description": "Founder: https://t.co/xt1Nz7k2Jo - the best way to hire amazing people \n\nPrevious: exits to @Indeed @Zillow \n\nNewsletter about tech, memes, and careers: https://t.co/d826eonRL6",
+			"url": "https://t.co/Gt3UFaYOPE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Gt3UFaYOPE",
+							"expanded_url": "https://www.linkedin.com/in/bakk3/",
+							"display_url": "linkedin.com/in/bakk3/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/xt1Nz7k2Jo",
+							"expanded_url": "http://Laskie.com",
+							"display_url": "Laskie.com",
+							"indices": [
+								9,
+								32
+							]
+						},
+						{
+							"url": "https://t.co/d826eonRL6",
+							"expanded_url": "http://chrisbakke.com",
+							"display_url": "chrisbakke.com",
+							"indices": [
+								153,
+								176
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 90372,
+			"friends_count": 159,
+			"listed_count": 927,
+			"created_at": "Thu Apr 18 05:57:04 +0000 2013",
+			"favourites_count": 31204,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2931,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491961095136763905/Sd8XRL5__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491961095136763905/Sd8XRL5__normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14522,
+		"favorite_count": 103801,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This guy wins at Linkedin https://t.co/EPaJLROGtY"
+	}
+]
