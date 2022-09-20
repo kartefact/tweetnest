286e479223869d0fd41f4fa15e106bbd3a02614d
Karthikeyan Ravikumar <kartefact@gmail.com>
2022-09-20 05:31:31 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101222,
-	"created_at": "Thu Sep 01 00:01:31 +0000 2022",
+	"tweets": 101223,
+	"created_at": "Tue Sep 20 00:01:31 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_09.js
@@ -0,0 +1,188 @@
+Grailbird.data.tweets_2022_09 = 
+[
+	{
+		"created_at": "Mon Sep 19 07:10:54 +0000 2022",
+		"id": 1571550468353949700,
+		"id_str": "1571758715820453888",
+		"full_text": "On 29th October 2002, Microsoft released Windows XP Media Center Edition (MCE). Designed for PCs used for home entertainment, it introduced Windows Media Center, which acted as a hub &amp; player for the user’s music &amp; video. MCE also sported a brand new default visual style, Royale. https://t.co/TObwR52433",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			288
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1571550464168149000,
+					"id_str": "1571550464168148995",
+					"indices": [
+						289,
+						312
+					],
+					"media_url": "http://pbs.twimg.com/media/Fc9E0htXwAMMpq-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fc9E0htXwAMMpq-.jpg",
+					"url": "https://t.co/TObwR52433",
+					"display_url": "pic.twitter.com/TObwR52433",
+					"expanded_url": "https://twitter.com/wowstartsnow/status/1571550468353949696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1571550464168149000,
+					"id_str": "1571550464168148995",
+					"indices": [
+						289,
+						312
+					],
+					"media_url": "http://pbs.twimg.com/media/Fc9E0htXwAMMpq-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fc9E0htXwAMMpq-.jpg",
+					"url": "https://t.co/TObwR52433",
+					"display_url": "pic.twitter.com/TObwR52433",
+					"expanded_url": "https://twitter.com/wowstartsnow/status/1571550468353949696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2603326764,
+			"id_str": "2603326764",
+			"name": "Windows On Windows",
+			"screen_name": "wowstartsnow",
+			"location": "United Kingdom 🇬🇧",
+			"description": "The YouTube channel on the history & development of #Microsoft #Windows. 📺 Join the Discord @ https://t.co/qsaddkn3ed! 👨🏻‍💻 Follow for daily Windows facts. 🤓",
+			"url": "https://t.co/rIPqokZmfz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rIPqokZmfz",
+							"expanded_url": "http://youtube.com/windowsonwindows",
+							"display_url": "youtube.com/windowsonwindo…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/qsaddkn3ed",
+							"expanded_url": "http://discord.io/wowserv",
+							"display_url": "discord.io/wowserv",
+							"indices": [
+								94,
+								117
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8461,
+			"friends_count": 1242,
+			"listed_count": 88,
+			"created_at": "Fri Jul 04 11:51:46 +0000 2014",
+			"favourites_count": 11654,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4901,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "592E7D",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1567929686675554307/oTrvqQci_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1567929686675554307/oTrvqQci_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2603326764/1581855900",
+			"profile_link_color": "5A2E7D",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 40,
+		"favorite_count": 268,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On 29th October 2002, Microsoft released Windows XP Media Center Edition (MCE). Designed for PCs used for home entertainment, it introduced Windows Media Center, which acted as a hub &amp; player for the user’s music &amp; video. MCE also sported a brand new default visual style, Royale. https://t.co/TObwR52433"
+	}
+]
