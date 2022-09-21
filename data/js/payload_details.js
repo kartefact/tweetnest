d8390edeb4016653d575fad1f8e11355a17d2d51
Karthikeyan Ravikumar <kartefact@gmail.com>
2022-09-21 05:31:32 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101223,
-	"created_at": "Tue Sep 20 00:01:31 +0000 2022",
+	"tweets": 101224,
+	"created_at": "Wed Sep 21 00:01:31 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Tue Sep 20 06:21:05 +0000 2022",
+		"id": 1572108568970821600,
+		"id_str": "1572108568970821632",
+		"full_text": "@r3dash Now that S21FE is at 40k (before the bank offers) in BBD sale, would you recommend it over Moto edge 30 fusion at a similar/same price?",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "r3dash",
+					"name": "Ershad Kaleebullah",
+					"id": 106728983,
+					"id_str": "106728983",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 106728983,
+		"in_reply_to_user_id_str": "106728983",
+		"in_reply_to_screen_name": "r3dash",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1391,
+			"friends_count": 224,
+			"listed_count": 44,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10178,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 98964,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@r3dash Now that S21FE is at 40k (before the bank offers) in BBD sale, would you recommend it over Moto edge 30 fusion at a similar/same price?"
+	},
+	{
 		"created_at": "Mon Sep 19 07:10:54 +0000 2022",
 		"id": 1571550468353949700,
 		"id_str": "1571758715820453888",
@@ -106,7 +200,7 @@ Grailbird.data.tweets_2022_09 =
 			"name": "Windows On Windows",
 			"screen_name": "wowstartsnow",
 			"location": "United Kingdom 🇬🇧",
-			"description": "The YouTube channel on the history & development of #Microsoft #Windows. 📺 Join the Discord @ https://t.co/qsaddkn3ed! 👨🏻‍💻 Follow for daily Windows facts. 🤓",
+			"description": "The YouTube channel on the history & development of #Microsoft #Windows. 📺 Join the Discord @ https://t.co/qsaddkn3ed! 👨🏻💻 Follow for daily Windows facts. 🤓",
 			"url": "https://t.co/rIPqokZmfz",
 			"entities": {
 				"url": {
