3f264819d00758b5c3980903534b7057995e320d
Karthikeyan Ravikumar <kartefact@gmail.com>
2022-09-22 05:31:32 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101224,
-	"created_at": "Wed Sep 21 00:01:31 +0000 2022",
+	"tweets": 101225,
+	"created_at": "Thu Sep 22 00:01:31 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,178 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 21 17:10:50 +0000 2022",
+		"id": 1572634470871863300,
+		"id_str": "1572634470871863298",
+		"full_text": "I am gifting you Google Share worth ₹1000. Enter my code KARCC61RNRGGL while signing up 🥳\nClick to claim today 👉🏼 https://t.co/mFsQBuzvhr. *Offer expires on 18-Oct-2022.* https://t.co/nx7W6Pq1Qy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/mFsQBuzvhr",
+					"expanded_url": "https://indmoney.onelink.me/RmHC/tfb1buil",
+					"display_url": "indmoney.onelink.me/RmHC/tfb1buil",
+					"indices": [
+						114,
+						137
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1572634467038269400,
+					"id_str": "1572634467038269440",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/FdMetzFaEAAK0XB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdMetzFaEAAK0XB.jpg",
+					"url": "https://t.co/nx7W6Pq1Qy",
+					"display_url": "pic.twitter.com/nx7W6Pq1Qy",
+					"expanded_url": "https://twitter.com/kartefact/status/1572634470871863298/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1572634467038269400,
+					"id_str": "1572634467038269440",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/FdMetzFaEAAK0XB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdMetzFaEAAK0XB.jpg",
+					"url": "https://t.co/nx7W6Pq1Qy",
+					"display_url": "pic.twitter.com/nx7W6Pq1Qy",
+					"expanded_url": "https://twitter.com/kartefact/status/1572634470871863298/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1391,
+			"friends_count": 224,
+			"listed_count": 44,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10178,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 98966,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I am gifting you Google Share worth ₹1000. Enter my code KARCC61RNRGGL while signing up 🥳\nClick to claim today 👉🏼 https://t.co/mFsQBuzvhr. *Offer expires on 18-Oct-2022.* https://t.co/nx7W6Pq1Qy"
+	},
+	{
 		"created_at": "Tue Sep 20 06:21:05 +0000 2022",
 		"id": 1572108568970821600,
 		"id_str": "1572108568970821632",
