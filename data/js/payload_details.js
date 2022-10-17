8e38b8ae7ef7ee7e006e9c720a71f7d4da1871df
Karthikeyan Ravikumar <kartefact@gmail.com>
2022-10-17 05:31:32 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101236,
-	"created_at": "Sun Oct 16 00:01:31 +0000 2022",
+	"tweets": 101237,
+	"created_at": "Mon Oct 17 00:01:31 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sun Oct 16 13:36:17 +0000 2022",
+		"id": 1581640173678231600,
+		"id_str": "1581640173678231552",
+		"full_text": "@Airtel_Presence The screenshot is from that link and I'm contacting you after checking with @motorolaindia . Can you both work it out between yourselves and then update all customers?",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			184
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Airtel_Presence",
+					"name": "Airtel Cares",
+					"id": 103323813,
+					"id_str": "103323813",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "motorolaindia",
+					"name": "Motorola India",
+					"id": 2460859956,
+					"id_str": "2460859956",
+					"indices": [
+						93,
+						107
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1581639202462392300,
+		"in_reply_to_status_id_str": "1581639202462392327",
+		"in_reply_to_user_id": 103323813,
+		"in_reply_to_user_id_str": "103323813",
+		"in_reply_to_screen_name": "Airtel_Presence",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1391,
+			"friends_count": 224,
+			"listed_count": 44,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10187,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 98980,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Airtel_Presence The screenshot is from that link and I'm contacting you after checking with @motorolaindia . Can you both work it out between yourselves and then update all customers?"
+	},
+	{
 		"created_at": "Sat Oct 15 00:28:37 +0000 2022",
 		"id": 1581079561651236900,
 		"id_str": "1581079561651236865",
