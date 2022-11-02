1452901dfb11edb09413f8599812bbfebcbace76
Karthikeyan Ravikumar <kartefact@gmail.com>
2022-11-02 05:31:32 +0530

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101252,
-	"created_at": "Tue Nov 01 00:01:31 +0000 2022",
+	"tweets": 101255,
+	"created_at": "Wed Nov 02 00:01:31 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_11.js
@@ -0,0 +1,463 @@
+Grailbird.data.tweets_2022_11 = 
+[
+	{
+		"created_at": "Tue Nov 01 04:15:48 +0000 2022",
+		"id": 1587297330155311000,
+		"id_str": "1587297330155311104",
+		"full_text": "I was going through my app list and to my surprise, found that the Orkut app still exists on the Play Store. #DidYouKnow ? https://t.co/FTIpw98lm0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DidYouKnow",
+					"indices": [
+						109,
+						120
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1587297327399596000,
+					"id_str": "1587297327399596035",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/Fgc2gmiUcAM-90S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fgc2gmiUcAM-90S.jpg",
+					"url": "https://t.co/FTIpw98lm0",
+					"display_url": "pic.twitter.com/FTIpw98lm0",
+					"expanded_url": "https://twitter.com/kartefact/status/1587297330155311104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 306,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 922,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1587297327399596000,
+					"id_str": "1587297327399596035",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/Fgc2gmiUcAM-90S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fgc2gmiUcAM-90S.jpg",
+					"url": "https://t.co/FTIpw98lm0",
+					"display_url": "pic.twitter.com/FTIpw98lm0",
+					"expanded_url": "https://twitter.com/kartefact/status/1587297330155311104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 306,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 922,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1386,
+			"friends_count": 227,
+			"listed_count": 44,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10192,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 98980,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I was going through my app list and to my surprise, found that the Orkut app still exists on the Play Store. #DidYouKnow ? https://t.co/FTIpw98lm0"
+	},
+	{
+		"created_at": "Tue Nov 01 03:56:08 +0000 2022",
+		"id": 1587292378498740200,
+		"id_str": "1587292378498740224",
+		"full_text": "@lalitpatnaik ...but when they say \"we\", I think they're referring only to the operator of the airport. Flights are operated by \"other\" companies, isn't it?",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			156
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lalitpatnaik",
+					"name": "Lalit Patnaik",
+					"id": 140501827,
+					"id_str": "140501827",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1587021818434560000,
+		"in_reply_to_status_id_str": "1587021818434560000",
+		"in_reply_to_user_id": 140501827,
+		"in_reply_to_user_id_str": "140501827",
+		"in_reply_to_screen_name": "lalitpatnaik",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1386,
+			"friends_count": 227,
+			"listed_count": 44,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10192,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 98980,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@lalitpatnaik ...but when they say \"we\", I think they're referring only to the operator of the airport. Flights are operated by \"other\" companies, isn't it?"
+	},
+	{
+		"created_at": "Tue Nov 01 03:34:57 +0000 2022",
+		"id": 1587287050171994000,
+		"id_str": "1587287050171994112",
+		"full_text": "@scycer @sudhamshu In our days, was used to be more alert than our parents. We would know that it's a holiday and get up early voluntarily to do masti. https://t.co/6LrVTpNY9z",
+		"truncated": false,
+		"display_text_range": [
+			19,
+			151
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "scycer",
+					"name": "sidharth sam",
+					"id": 34255548,
+					"id_str": "34255548",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "sudhamshu",
+					"name": "Nobody",
+					"id": 15105039,
+					"id_str": "15105039",
+					"indices": [
+						8,
+						18
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1587287039472377900,
+					"id_str": "1587287039472377857",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FgctJxBVIAEJZXf.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FgctJxBVIAEJZXf.jpg",
+					"url": "https://t.co/6LrVTpNY9z",
+					"display_url": "pic.twitter.com/6LrVTpNY9z",
+					"expanded_url": "https://twitter.com/kartefact/status/1587287050171994112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 220,
+							"h": 238,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 220,
+							"h": 238,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 220,
+							"h": 238,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1587287039472377900,
+					"id_str": "1587287039472377857",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FgctJxBVIAEJZXf.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FgctJxBVIAEJZXf.jpg",
+					"url": "https://t.co/6LrVTpNY9z",
+					"display_url": "pic.twitter.com/6LrVTpNY9z",
+					"expanded_url": "https://twitter.com/kartefact/status/1587287050171994112/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 220,
+							"h": 238,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 220,
+							"h": 238,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 220,
+							"h": 238,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							110,
+							119
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FgctJxBVIAEJZXf.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1587280872868151300,
+		"in_reply_to_status_id_str": "1587280872868151297",
+		"in_reply_to_user_id": 34255548,
+		"in_reply_to_user_id_str": "34255548",
+		"in_reply_to_screen_name": "scycer",
+		"user": {
+			"id": 35215066,
+			"id_str": "35215066",
+			"name": "Kartefact",
+			"screen_name": "kartefact",
+			"location": "Bengaluru, India",
+			"description": "For all your days be prepared, and meet them ever alike. When you are the anvil, bear - when you are the hammer, strike. - Edwin Markham",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1386,
+			"friends_count": 227,
+			"listed_count": 44,
+			"created_at": "Sat Apr 25 13:53:50 +0000 2009",
+			"favourites_count": 10192,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 98980,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084029727864770561/L4OEfUJ6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35215066/1565329647",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@scycer @sudhamshu In our days, was used to be more alert than our parents. We would know that it's a holiday and get up early voluntarily to do masti. https://t.co/6LrVTpNY9z"
+	}
+]
