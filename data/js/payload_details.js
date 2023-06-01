d5efa61b5f8d7a72bb0b21187f9be57f43625556
Karthikeyan Ravikumar <kartefact@gmail.com>
2023-06-01 05:31:32 +0530

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 101262,
-	"created_at": "Tue May 16 00:01:31 +0000 2023",
+	"tweets": 101263,
+	"created_at": "Thu Jun 01 00:01:31 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Wed May 31 02:56:27 +0000 2023",
+		"id": 1663559722145058800,
+		"id_str": "1663741201189867520",
+		"full_text": "Your monthly ABAP + Excel SAP Community blog post. Now with 3 parts!  #SAP #ABAP\nhttps://t.co/6H41Lahuao https://t.co/6vX9YyBxEe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SAP",
+					"indices": [
+						70,
+						74
+					]
+				},
+				{
+					"text": "ABAP",
+					"indices": [
+						75,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6H41Lahuao",
+					"expanded_url": "https://blogs.sap.com/2023/05/25/excel-mass-upload-in-background-job-using-shma-part-1-create-a-custom-shared-memory-area/",
+					"display_url": "blogs.sap.com/2023/05/25/exc…",
+					"indices": [
+						81,
+						104
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1663559610387816400,
+					"id_str": "1663559610387816453",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/FxYmptfaYAUaqEk.png",
+					"media_url_https": "https://pbs.twimg.com/media/FxYmptfaYAUaqEk.png",
+					"url": "https://t.co/6vX9YyBxEe",
+					"display_url": "pic.twitter.com/6vX9YyBxEe",
+					"expanded_url": "https://twitter.com/JelenaAtLarge/status/1663559722145058817/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 734,
+							"h": 409,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 379,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 734,
+							"h": 409,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1663559610387816400,
+					"id_str": "1663559610387816453",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/FxYmptfaYAUaqEk.png",
+					"media_url_https": "https://pbs.twimg.com/media/FxYmptfaYAUaqEk.png",
+					"url": "https://t.co/6vX9YyBxEe",
+					"display_url": "pic.twitter.com/6vX9YyBxEe",
+					"expanded_url": "https://twitter.com/JelenaAtLarge/status/1663559722145058817/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 734,
+							"h": 409,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 379,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 734,
+							"h": 409,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 908762540334878700,
+			"id_str": "908762540334878720",
+			"name": "Jelena Perfiljeva",
+			"screen_name": "JelenaAtLarge",
+			"location": "North Carolina, USA",
+			"description": "SAP celebrity of sorts. Recovering ABAPosaurus. Writer. Expert SAP Developer @MindsetERP. Chief Nerdess @BoringNerds she/her #SAPMentorAlum #SAP #ABAP #ENSFW",
+			"url": "https://t.co/T4DeFUTD32",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/T4DeFUTD32",
+							"expanded_url": "https://boringenterprisenerds.com/",
+							"display_url": "boringenterprisenerds.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3562,
+			"friends_count": 146,
+			"listed_count": 37,
+			"created_at": "Fri Sep 15 18:40:48 +0000 2017",
+			"favourites_count": 16127,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7110,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/912021977380147202/ppa9bkbT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/912021977380147202/ppa9bkbT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/908762540334878720/1653439326",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Your monthly ABAP + Excel SAP Community blog post. Now with 3 parts!  #SAP #ABAP\nhttps://t.co/6H41Lahuao https://t.co/6vX9YyBxEe"
+	},
+	{
 		"created_at": "Mon May 15 13:41:33 +0000 2023",
 		"id": 1657412287852888000,
 		"id_str": "1658105337441054720",
